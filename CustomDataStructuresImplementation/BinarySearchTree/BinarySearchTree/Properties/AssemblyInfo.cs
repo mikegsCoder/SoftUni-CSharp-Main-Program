64788049d34c14ced4e120ec2abf799cb8756c9e
Mihail Sidov <mikegsid@gmail.com>
2021-04-15 16:58:56 +0300

    Added more methods in custom implemented Binary Search Tree

--- CustomDataStructuresImplementation/BinarySearchTree/BinarySearchTree/Properties/AssemblyInfo.cs
+++ CustomDataStructuresImplementation/BinarySearchTree/BinarySearchTree/Properties/AssemblyInfo.cs
@@ -2,34 +2,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Trees")]
+[assembly: AssemblyTitle("BinarySearchTree")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Trees")]
+[assembly: AssemblyProduct("BinarySearchTree")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0a73a822-c139-4698-ae8b-3a789b6a22cc")]
+[assembly: Guid("535b6a0a-8ed5-4580-abae-cd7e251578f8")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
