4ee636879e7ef5b20604aabaf35a1c1dd20ec35a
Mihail Sidov <mikegsid@gmail.com>
2021-05-05 16:31:04 +0300

    Added Faster Queue custom data structure implementation

---
+++ CustomDataStructuresImplementation/FasterQueue/FastQueue.cs
@@ -0,0 +1,94 @@
+﻿namespace Problem01.FasterQueue
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class FastQueue<T> : IAbstractQueue<T>
+    {
+        private Node<T> _head;
+        private Node<T> _tail;
+        public int Count { get; private set; }
+
+        public bool Contains(T item)
+        {
+            var current = this._head;
+
+            while (current != null)
+            {
+                if (current.Item.Equals(item))
+                {
+                    return true;
+                }
+
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+        public T Dequeue()
+        {
+            this.EnsureNotEmpty();
+
+            var headItem = this._head.Item;
+            var newHead = this._head.Next;
+            this._head.Next = null;
+            this._head = newHead;
+
+            this.Count--;
+
+            return headItem;
+        }
+
+        public void Enqueue(T item)
+        {
+            var newNode = new Node<T>
+            {
+                Item = item,
+                Next = null
+            };
+
+            if (this._head is null)
+            {
+                this._head = newNode;
+                this._tail = newNode;
+            }
+            else
+            {
+                var newTail = newNode;
+                this._tail.Next = newNode;
+                this._tail = newTail;
+            }
+
+            this.Count++;
+        }
+
+        public T Peek()
+        {
+            this.EnsureNotEmpty();
+
+            return this._head.Item;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var current = this._head;
+            while (current != null)
+            {
+                yield return current.Item;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+        private void EnsureNotEmpty()
+        {
+            if (this.Count == 0)
+                throw new InvalidOperationException();
+        }
+    }
+}
