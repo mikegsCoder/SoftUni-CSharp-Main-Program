c35151ec1efbb694311248e971af4053965d7b8c
Mihail Sidov <mikegsid@gmail.com>
2021-06-12 11:55:14 +0300

    Added new problem solutions in Reflection and Attributes in C# OOP

---
+++ CSharpOOP/Reflection-and-Attributes/Stealer/Spy.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string investigatedClass, params string[] requestedFields)
+        {
+            Type classType = Type.GetType(investigatedClass);
+            FieldInfo[] classFields = classType.GetFields
+                (BindingFlags.Instance | 
+                BindingFlags.NonPublic | 
+                BindingFlags.Public | 
+                BindingFlags.Static);
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            Object classInstance = Activator.CreateInstance(classType, new object[] { });
+
+            stringBuilder.AppendLine($"Class under investigation: {investigatedClass}");
+
+            foreach (FieldInfo field in classFields.Where(f => requestedFields.Contains(f.Name)))
+            {
+                stringBuilder.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+
+            return stringBuilder.ToString().Trim();
+        }
+
+        public string AnalyzeAcessModifiers(string className)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            Type classType = Type.GetType(className);
+
+            FieldInfo[] classFields = classType.GetFields
+                (BindingFlags.Instance |
+                BindingFlags.Public |
+                BindingFlags.Static);
+
+            MethodInfo[] classPublicMethods = classType.GetMethods
+                (BindingFlags.Instance | 
+                BindingFlags.Public);
+
+            MethodInfo[] classNonPublicMethods = classType.GetMethods
+                (BindingFlags.NonPublic | 
+                BindingFlags.Instance);
+
+            foreach (FieldInfo field in classFields)
+            {
+                sb.AppendLine($"{field.Name} must be private!");
+            }
+
+            foreach (MethodInfo method in classNonPublicMethods.Where(m => m.Name.StartsWith("get")))
+            {
+                sb.AppendLine($"{method.Name} have to be public!");
+            }
+
+            foreach (MethodInfo method in classPublicMethods.Where(m => m.Name.StartsWith("set")))
+            {
+                sb.AppendLine($"{method.Name} have to be private!");
+            }
+
+            return sb.ToString().Trim();
+        }
+
+        public string RevealPrivateMethods(string className)
+        {
+            StringBuilder sb = new StringBuilder();
+            Type classType = Type.GetType(className);
+
+            MethodInfo[] classPrivateMethods = classType.GetMethods
+                (BindingFlags.NonPublic | BindingFlags.Instance);
+
+            sb.AppendLine($"All Private Methods of Class: {className}");
+            sb.AppendLine($"Base Class: {classType.BaseType.Name}");
+
+            foreach (var method in classPrivateMethods)
+            {
+                sb.AppendLine(method.Name);
+            }
+
+            return sb.ToString().Trim();
+        }
+
+        public string CollectGettersAndSetters(string className)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            Type classType = Type.GetType(className);
+
+            MethodInfo[] classMethods = classType.GetMethods
+                (BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+            foreach (var method in classMethods.Where(m => m.Name.StartsWith("get")))
+            {
+                sb.AppendLine($"{method.Name} will return {method.ReturnType}");
+            }
+
+            foreach (var method in classMethods.Where(m => m.Name.StartsWith("set")))
+            {
+                sb.AppendLine
+                    ($"{method.Name} will set field of {method.GetParameters().First().ParameterType}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ CSharpOOP/Reflection-and-Attributes/Stealer/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Stealer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            // P01 Stealer
+            //Spy spy = new Spy();
+            //string result = spy.StealFieldInfo("Stealer.Hacker", "username", "password");
+            //Console.WriteLine(result);
+
+            // P02 High Quality Mistakes
+            //Spy spy = new Spy();
+            //string result = spy.AnalyzeAcessModifiers("Stealer.Hacker");
+            //Console.WriteLine(result);
+
+            // P03 Mission Private Impossible
+            //Spy spy = new Spy();
+            //string result = spy.RevealPrivateMethods("Stealer.Hacker");
+            //Console.WriteLine(result);
+
+            // P04 Collector
+            Spy spy = new Spy();
+            string result = spy.CollectGettersAndSetters("Stealer.Hacker");
+            Console.WriteLine(result);
+        }
+    }
+}
