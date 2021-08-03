e3f9f02d7804e13cbf3580400531ce01a855ba58
Mihail Sidov <mikegsid@gmail.com>
2021-08-03 17:13:06 +0300

    Added new problem solutions in C# Advanced - Generics

---
+++ CSharpAdvanced/08. Generics/Lab/01. Box Of T/Box.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        List<T> elements;
+        public Box()
+        {
+            this.elements = new List<T>();
+        }
+
+        public int Count 
+        { 
+            get 
+            { 
+                return elements.Count; 
+            }
+        }
+
+        public void Add(T element)
+        {
+            this.elements.Add(element);
+        }
+
+        public T Remove()
+        {
+            T removedElement = elements[elements.Count-1];
+            elements.RemoveAt(elements.Count-1);
+            return removedElement;
+        }
+    }
+}
---
+++ CSharpAdvanced/08. Generics/Lab/02. Generic Array Creator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {            
+            int[] integers = ArrayCreator.Create(10, 33);
+            string[] strings = ArrayCreator.Create(10, "pesho");
+            Console.WriteLine(string.Join(' ',integers));
+            Console.WriteLine(string.Join(' ',strings));
+        }
+    }
+}
