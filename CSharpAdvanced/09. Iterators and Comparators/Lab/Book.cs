ae147bbfd9f1bc31ad981314df603477db691e2e
Mihail Sidov <mikegsid@gmail.com>
2021-08-04 16:06:25 +0300

    Added new problem solutions in C# Advanced - Iterators and Comparators

---
+++ CSharpAdvanced/09. Iterators and Comparators/Lab/Book.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace IteratorsAndComparators
+{
+    public class Book
+    {
+        public Book(string title, int year, params string[] authors)
+        {
+            this.Title = title;
+            this.Year = year;
+            this.Authors = authors.ToList();
+        }
+
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+
+        public List<string> Authors { get; set; }
+                
+        public override string ToString()
+        {
+            return $"{Title} - {Year}";
+        }
+    }
+}
---
+++ CSharpAdvanced/09. Iterators and Comparators/Lab/Library.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        public Library(params Book[] books)
+        {
+            this.Books = new SortedSet<Book>(books, new BookComparator());
+        }
+
+        public SortedSet<Book> Books { get; set; }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(Books.ToList());
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private int index = -1;
+            public LibraryIterator(List<Book> books)
+            {
+                this.Books = books;
+            }
+
+            public List<Book> Books { get; set; }
+
+            public Book Current => Books[index];
+
+            object IEnumerator.Current => Books[index];
+
+            public void Dispose() { }
+            
+            public bool MoveNext()
+            {
+                return ++index < Books.Count;
+            }
+
+            public void Reset()
+            {
+                index = -1;
+            }
+        }
+    }
+}
