61ce29ee820b14b106f3aed0d6404e837829dc3f
Mihail Sidov <mikegsid@gmail.com>
2021-08-06 16:27:56 +0300

    Added new problem solutions in C# OOP - Encapsulation

---
+++ CSharpOOP/02. Encapsulation/Lab/PersonsInfo/Team.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            firstTeam = new List<Person>();
+            reserveTeam = new List<Person>();
+            this.name = name;
+        }
+
+        public IReadOnlyCollection<Person> FirstTeam
+        {
+            get { return this.firstTeam.AsReadOnly(); }
+        }
+
+        public IReadOnlyCollection<Person> ReserveTeam
+        {
+            get { return this.reserveTeam.AsReadOnly(); }
+        }
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                firstTeam.Add(person);
+            }
+            else
+            {
+                reserveTeam.Add(person);
+            }
+        }
+    }
+}
