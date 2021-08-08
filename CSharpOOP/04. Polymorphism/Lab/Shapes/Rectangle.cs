ac66b0f919b8b32c9336aee79ff85606b69390b9
Mihail Sidov <mikegsid@gmail.com>
2021-08-08 11:56:04 +0300

    Added new problem solutions in C# OOP - Polymorphism

---
+++ CSharpOOP/04. Polymorphism/Lab/Shapes/Rectangle.cs
@@ -0,0 +1,40 @@
+﻿namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double width;
+        private double height;
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public double Width
+        {
+            get { return width; }
+            private set { width = value; }
+        }
+
+        public double Height
+        {
+            get { return height; }
+            private set { height = value; }
+        }
+        public override double CalculateArea()
+        {
+            return this.Height * this.Width;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * (this.Height + this.Width);
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + this.GetType().Name;
+        }
+    }
+}
---
+++ CSharpOOP/04. Polymorphism/Lab/Shapes/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int radius = int.Parse(Console.ReadLine());
+            Circle circle = new Circle(radius);
+
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            Rectangle rectangle = new Rectangle(width, height);
+
+            Console.WriteLine(circle.Draw());
+            Console.WriteLine(rectangle.Draw());
+        }
+    }
+}
