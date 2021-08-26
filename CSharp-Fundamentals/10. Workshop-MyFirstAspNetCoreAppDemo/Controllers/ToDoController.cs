6771ffcec18e46b68e6c3b09fc37239130b10747
Mihail Sidov <mikegsid@gmail.com>
2021-08-26 12:49:54 +0300

    Added new problem solutions

---
+++ CSharp-Fundamentals/10. Workshop-MyFirstAspNetCoreAppDemo/Controllers/ToDoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstAspNetCoreAppDemo.Controllers
+{
+    public class ToDoController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.HelloMessage = "Welcome to /ToDo/Index";
+            return View();
+        }
+    }
+}
