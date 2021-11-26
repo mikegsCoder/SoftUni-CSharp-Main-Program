e9b9896a686e39a6718cd0f721863be8435ff241
Mihail Sidov <mikegsid@gmail.com>
2021-11-26 18:10:26 +0200

    Add new problem solutions in Advenced Querying

---
+++ 06. Entity-Framework-Core/07. Advanced Querying/Lab/ModelsMusicX/SongMetadatum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Demo.ModelsMusicX
+{
+    public partial class SongMetadatum
+    {
+        public int Id { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
+
+        public int SongId { get; set; }
+
+        public short Type { get; set; }
+
+        public string Value { get; set; }
+
+        public int? SourceId { get; set; }
+
+        public string SourceItemId { get; set; }
+
+        public virtual Song Song { get; set; }
+
+        public virtual Source Source { get; set; }
+    }
+}
---
+++ 06. Entity-Framework-Core/07. Advanced Querying/Lab/ModelsSoftUni/EmployeesProject.cs
@@ -0,0 +1,13 @@
+﻿namespace Demo.ModelsSoftUni
+{
+    public partial class EmployeesProject
+    {
+        public int EmployeeId { get; set; }
+
+        public int ProjectId { get; set; }
+
+        public virtual Employee Employee { get; set; }
+
+        public virtual Project Project { get; set; }
+    }
+}
