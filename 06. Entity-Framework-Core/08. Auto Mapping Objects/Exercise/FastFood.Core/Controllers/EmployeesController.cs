a7efe7ece12c773bbf5e27224552b9a7927f3b64
Mihail Sidov <mikegsid@gmail.com>
2021-12-13 17:17:38 +0200

    Add new homework - problem solution in Auto Mapping - Exercise

---
+++ 06. Entity-Framework-Core/08. Auto Mapping Objects/Exercise/FastFood.Core/Controllers/EmployeesController.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq;
+using AutoMapper.QueryableExtensions;
+using FastFood.Core.ViewModels.Items;
+using FastFood.Core.ViewModels.Positions;
+using FastFood.Models;
+
+namespace FastFood.Core.Controllers
+{
+    using System;
+    using AutoMapper;
+    using Data;
+    using Microsoft.AspNetCore.Mvc;
+    using ViewModels.Employees;
+
+    public class EmployeesController : Controller
+    {
+        private readonly FastFoodContext context;
+        private readonly IMapper mapper;
+
+        public EmployeesController(FastFoodContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        public IActionResult Register()
+        {
+            var positions = this.context.Positions
+                .ProjectTo<RegisterEmployeeViewModel>(mapper.ConfigurationProvider)
+                .ToList();
+
+            return this.View(positions);
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterEmployeeInputModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            var employee = this.mapper.Map<Employee>(model);
+
+            this.context.Employees.Add(employee);
+
+            this.context.SaveChanges();
+
+            return RedirectToAction("All", "Employees");
+        }
+
+        public IActionResult All()
+        {
+            var employees = this.context.Employees
+                .ProjectTo<EmployeesAllViewModel>(mapper.ConfigurationProvider)
+                .ToList();
+
+            return this.View(employees);
+        }
+    }
+}
---
+++ 06. Entity-Framework-Core/08. Auto Mapping Objects/Exercise/FastFood.Core/Controllers/OrdersController.cs
@@ -0,0 +1,64 @@
+﻿using AutoMapper.QueryableExtensions;
+using FastFood.Core.ViewModels.Positions;
+using FastFood.Models;
+
+namespace FastFood.Core.Controllers
+{
+    using System;
+    using System.Linq;
+    using AutoMapper;
+    using Data;
+    using Microsoft.AspNetCore.Mvc;
+    using ViewModels.Orders;
+
+    public class OrdersController : Controller
+    {
+        private readonly FastFoodContext context;
+        private readonly IMapper mapper;
+
+        public OrdersController(FastFoodContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        public IActionResult Create()
+        {
+            var viewOrder = new CreateOrderViewModel
+            {
+                Items = this.context.Items.Select(x => x.Id).ToList(),
+                Employees = this.context.Employees.Select(x => x.Id).ToList(),
+            };
+
+            return this.View(viewOrder);
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateOrderInputModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            var order = this.mapper.Map<Order>(model);
+            var orderItem = this.mapper.Map<OrderItem>(model);
+            orderItem.OrderId = order.Id;
+
+            this.context.Orders.Add(order);
+            this.context.OrderItems.Add(orderItem);
+            this.context.SaveChanges();
+
+            return this.RedirectToAction("All", "Orders");
+        }
+
+        public IActionResult All()
+        {
+            var orders = this.context.Orders
+                .ProjectTo<OrderAllViewModel>(mapper.ConfigurationProvider)
+                .ToList();
+
+            return this.View(orders);
+        }
+    }
+}
