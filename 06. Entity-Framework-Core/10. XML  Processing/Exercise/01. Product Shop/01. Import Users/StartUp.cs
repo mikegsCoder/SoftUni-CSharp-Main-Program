8fa76a373c68b70c45b58727c2815cd10aa0d2a0
Mihail Sidov <mikegsid@gmail.com>
2021-12-27 11:11:18 +0200

    Add new homework - problem solution in XML Processing - Exercise

---
+++ 06. Entity-Framework-Core/10. XML  Processing/Exercise/01. Product Shop/01. Import Users/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace ProductShop
+{
+    using ProductShop.Utilities;
+
+    using Data;
+    using Dtos.Import;
+    using Models;
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var context = new ProductShopContext();
+            var xml = File.ReadAllText("../../../Datasets/users.xml");
+            var result = ImportUsers(context, xml);
+            Console.WriteLine(result);
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputXml)
+        {            
+            const string root = "Users";
+            UserInputModel[] userInputModels = XmlConverter.Deserializer<UserInputModel>(inputXml, root);
+            User[] users = userInputModels
+                .Select(x => new User
+                {
+                    FirstName = x.FirstName,
+                    LastName = x.LastName,
+                    Age = x?.Age,
+                }).ToArray();
+
+            context.AddRange((User[])users);
+            context.SaveChanges();
+            var result = $"Successfully imported {users.Length}";
+
+            return result;
+        }
+    }
+}
