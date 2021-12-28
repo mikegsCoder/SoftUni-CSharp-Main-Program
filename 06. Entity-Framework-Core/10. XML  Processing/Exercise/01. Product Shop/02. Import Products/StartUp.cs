bc32163281de00b39c86e80ac0aba6dfd38ae903
Mihail Sidov <mikegsid@gmail.com>
2021-12-28 11:13:34 +0200

    Add new homework - problem solution in XML Processing - Exercise

---
+++ 06. Entity-Framework-Core/10. XML  Processing/Exercise/01. Product Shop/02. Import Products/StartUp.cs
@@ -0,0 +1,43 @@
+﻿namespace ProductShop
+{
+    using ProductShop.Dtos.Import;
+    using ProductShop.Utillities;
+
+    using Data;
+    using Models;
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var context = new ProductShopContext();
+            var xml = File.ReadAllText("../../../Datasets/products.xml");
+            var result = ImportProducts(context, xml);
+
+            Console.WriteLine(result);
+        }
+
+        public static string ImportProducts(ProductShopContext context, string inputXml)
+        {            
+            const string root = "Products";
+            ProductInputModel[] productInputModels = XmlConverter.Deserializer<ProductInputModel>(inputXml, root);
+            Product[] products = productInputModels
+                .Select(x => new Product()
+                {
+                    Name = x.Name,
+                    Price = x.Price,
+                    SellerId = x.SellerId,
+                    BuyerId = x?.BuyerId,
+                }).ToArray();
+
+            context.AddRange((Product[])products);
+            context.SaveChanges();
+            var result = $"Successfully imported {products.Length}";
+
+            return result;
+        }
+    }
+}
