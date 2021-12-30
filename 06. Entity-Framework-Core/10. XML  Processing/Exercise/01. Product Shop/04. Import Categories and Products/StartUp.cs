043468a2262c47ae4674e0efd7fbe12ff5d7c441
Mihail Sidov <mikegsid@gmail.com>
2021-12-30 13:40:39 +0200

    Add new homework - problem solution in XML Processing - exercise

---
+++ 06. Entity-Framework-Core/10. XML  Processing/Exercise/01. Product Shop/04. Import Categories and Products/StartUp.cs
@@ -0,0 +1,47 @@
+﻿namespace ProductShop
+{
+    using ProductShop.Dtos.Import;
+    using ProductShop.Utillities;
+
+    using Data;
+    using Models;
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var context = new ProductShopContext();
+            var xml = File.ReadAllText("../../../Datasets/categories-products.xml");
+            var result = ImportCategoryProducts(context, xml);
+
+            Console.WriteLine(result);
+        }
+
+        public static string ImportCategoryProducts(ProductShopContext context, string inputXml)
+        {
+            var categories = context.Categories.Select(x => x.Id).ToList();
+            var products = context.Products.Select(x => x.Id).ToList();
+                        
+            const string root = "CategoryProducts";
+            CategoryProductInputModel[] categoryProductInputModels = XmlConverter.Deserializer<CategoryProductInputModel>(inputXml, root);
+
+            CategoryProduct[] categoryProducts = categoryProductInputModels
+                .Where(x => categories.Contains(x.CategoryId) && products.Contains(x.ProductId))
+                .Select(x => new CategoryProduct()
+                {
+                    CategoryId = x.CategoryId,
+                    ProductId = x.ProductId
+                })
+                .ToArray();
+
+            context.AddRange((CategoryProduct[])categoryProducts);
+            context.SaveChanges();
+            var result = $"Successfully imported {categoryProducts.Length}";
+
+            return result;
+        }
+    }
+}
