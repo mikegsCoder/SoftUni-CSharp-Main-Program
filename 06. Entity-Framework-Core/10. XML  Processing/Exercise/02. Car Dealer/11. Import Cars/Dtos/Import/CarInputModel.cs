9d8130d6caed64226af7fdae31ea728ced4889ed
Mihail Sidov <mikegsid@gmail.com>
2022-01-06 17:30:15 +0200

    Add new homework - problem solution in XML Processing - Exercise

---
+++ 06. Entity-Framework-Core/10. XML  Processing/Exercise/02. Car Dealer/11. Import Cars/Dtos/Import/CarInputModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace CarDealer.Dtos.Import
+{
+    using CarDealer.Models;
+    using System.Collections.Generic;
+    using System.Xml.Serialization;
+
+    [XmlType("Car")]
+    public class CarInputModel
+    {
+        [XmlElement("make")]
+        public string Make { get; set; }
+
+        [XmlElement("model")]
+        public string Model { get; set; }
+
+        [XmlElement("TraveledDistance")]
+        public long TravelledDistance { get; set; }
+
+        [XmlArrayItem("partId", IsNullable = false)]
+        public CarsCarPartId[] parts { get; set; }
+
+        [Serializable()]
+        [DesignerCategory("code")]
+        [XmlType(AnonymousType = true)]
+        public class CarsCarPartId
+        {
+            [XmlAttribute()]
+            public byte id { get; set; }
+        }
+    }
+}
