a691b1e8305b2915371d2415c7a2bf79cc543a68
Mihail Sidov <mikegsid@gmail.com>
2022-01-09 12:11:36 +0200

    Add new homework - problem solution in XML Processing - Exercise

---
+++ 06. Entity-Framework-Core/10. XML  Processing/Exercise/02. Car Dealer/14 . Export Cars With Distance/StartUp.cs
@@ -0,0 +1,46 @@
+﻿namespace CarDealer
+{
+    using CarDealer.Data;
+    using CarDealer.Dtos.Export;
+
+    using System;
+    using System.IO;
+    using System.Xml.Serialization;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            CarDealerContext context = new CarDealerContext();
+            string result = GetCarsWithDistance(context);
+
+            Console.WriteLine(result);
+        }
+
+        public static string GetCarsWithDistance(CarDealerContext context)
+        {
+            var cars = context
+                .Cars
+                .Where(x => x.TravelledDistance > 2_000_000)
+                .Select(x => new CarOutputModel
+                {
+                    Make = x.Make,
+                    Model = x.Model,
+                    TravelledDistance = x.TravelledDistance
+                })
+                .OrderBy(x => x.Make)
+                .ThenBy(x => x.Model)
+                .Take(10)
+                .ToArray();
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(CarOutputModel[]), new XmlRootAttribute("cars"));
+            var textWriter = new StringWriter();
+            var ns = new XmlSerializerNamespaces();
+            ns.Add("", "");
+            xmlSerializer.Serialize(textWriter, cars, ns);
+            var result = textWriter.ToString();
+
+            return result;
+        }
+    }
+}
