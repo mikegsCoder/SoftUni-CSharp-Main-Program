b851a20f70acfbab2c9e7130740f8cb1eb648f9b
Mihail Sidov <mikegsid@gmail.com>
2022-01-10 18:23:55 +0200

    Add new homework - problem solution in XML Processing - exercise

---
+++ 06. Entity-Framework-Core/10. XML  Processing/Exercise/02. Car Dealer/15. Export Cars From Make BMW/StartUp.cs
@@ -0,0 +1,57 @@
+﻿namespace CarDealer
+{
+    using CarDealer.Data;
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Xml.Linq;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            CarDealerContext context = new CarDealerContext();
+            string result = GetCarsFromMakeBmw(context);
+            //File.WriteAllText("../../../r.txt", result);
+
+            Console.WriteLine(result);
+        }
+
+        public static string GetCarsFromMakeBmw(CarDealerContext context)
+        {
+            var cars = context
+                .Cars
+                .Where(x => x.Make == "BMW")
+                .Select(x => new
+                {
+                    x.Id,
+                    x.Make,
+                    x.Model,
+                    x.TravelledDistance
+                })
+                .OrderBy(x => x.Model)
+                .ThenByDescending(x=>x.TravelledDistance)
+                .ToList();
+
+            XDocument doc = new XDocument();
+            doc.Declaration = new XDeclaration("1.0", "UTF-8", null);
+            doc.Add(new XElement("cars"));
+
+            foreach (var car in cars)
+            {
+                XElement el =
+                    new XElement("car",
+                        new XAttribute("id", car.Id),
+                        new XAttribute("model", car.Model),
+                        new XAttribute("travelled-distance", car.TravelledDistance));
+
+                doc.Root.Add(el);
+            }
+
+            var wr = new StringWriter();
+            doc.Save(wr);
+
+            return wr.ToString();
+        }
+    }
+}
