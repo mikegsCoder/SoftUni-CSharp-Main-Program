84729f13dca602194b635bb1cc1aa01abc88a367
mikegsCoder <mikegsid@gmail.com>
2022-01-17 18:26:32 +0200

    Add new problem solutions in Best Practices And Architecture

---
+++ 06. Entity-Framework-Core/11. Best Practices And Architecture/01. RealEstate/RealEstates.Data/ApplicationDbContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RealEstates.Models;
+
+namespace RealEstates.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Property> Properties { get; set; }
+
+        public DbSet<BuildingType> BuildingTypes { get; set; }
+
+        public DbSet<District> Districts { get; set; }
+
+        public DbSet<PropertyType> PropertyTypes { get; set; }
+
+        public DbSet<Tag> Tags { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=RealEstates;Integrated security=true");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ 06. Entity-Framework-Core/11. Best Practices And Architecture/01. RealEstate/RealEstates.Importer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using RealEstates.Data;
+using RealEstates.Services;
+
+namespace RealEstates.Importer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var apartmentsJson = "apartments.json";
+            var housesJson = "houses.json";
+
+            ImportJson(apartmentsJson);
+            Console.WriteLine();
+            ImportJson(housesJson);
+        }
+
+        private static void ImportJson(string fileName)
+        {
+            var dbContext = new ApplicationDbContext();
+            IPropertiesService propertiesService = new PropertiesService(dbContext);
+
+            IEnumerable<PropertyAsJson> properties = JsonSerializer.Deserialize<IEnumerable<PropertyAsJson>>(File.ReadAllText(fileName));
+            foreach (var jsonProp in properties)
+            {
+                propertiesService.Add(
+                    jsonProp.District,
+                    jsonProp.Floor,
+                    jsonProp.TotalFloors,
+                    jsonProp.Size,
+                    jsonProp.YardSize,
+                    jsonProp.Year,
+                    jsonProp.Type,
+                    jsonProp.BuildingType,
+                    jsonProp.Price);
+                Console.Write('.');
+            }
+        }
+    }
+}
