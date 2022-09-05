9057d33337541f879b3d162ee5bd70114723bc3a
mikegsCoder <mikegsid@gmail.com>
2022-09-05 11:49:29 +0300

    Add ReadRequest method in HttpServer

--- 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Server/HttpServer.cs
+++ 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Server/HttpServer.cs
@@ -19,6 +19,32 @@ namespace BasicWebServer.Server
             this.serverListener = new TcpListener(this.ipAddress, port);
         }
 
+        private string ReadRequest(NetworkStream networkStream)
+        {
+            var bufferLength = 1024;
+            var buffer = new byte[bufferLength];
 
+            var totalBytes = 0;
+
+            var requestBuilder = new StringBuilder();
+
+            do
+            {
+                var bytesRead = networkStream.Read(buffer, 0, bufferLength);
+
+                totalBytes += bytesRead;
+
+                if (totalBytes > 10 * 1024)
+                {
+                    throw new InvalidOperationException("Request is too large.");
+                }
+
+                requestBuilder.Append(Encoding.UTF8.GetString(buffer, 0, bytesRead));
+            }
+
+            while (networkStream.DataAvailable);
+
+            return requestBuilder.ToString();
+        }
     }
 }
