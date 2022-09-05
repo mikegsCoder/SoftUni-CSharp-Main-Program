cb0674bc3a6a270a207c222876ad2e0f0ec917ef
mikegsCoder <mikegsid@gmail.com>
2022-09-05 11:56:11 +0300

    Initialize new instance of HttpServer and start it

--- 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Demo/Startup.cs
+++ 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Demo/Startup.cs
@@ -6,7 +6,8 @@ namespace BasicWebServer.Demo
     {
         public static void Main()
         {
-            
+            var server = new HttpServer("127.0.0.1", 8080);
+            server.Start();
         }
     }
 }
