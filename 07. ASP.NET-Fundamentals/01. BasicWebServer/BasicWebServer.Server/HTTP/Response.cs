cd0efa7352c465e6ebc696828e4f7663a34f9b8f
mikegsCoder <mikegsid@gmail.com>
2022-09-06 09:39:36 +0300

    Add Response

---
+++ 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Server/HTTP/Response.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BasicWebServer.Server.HTTP
+{
+    public class Response
+    {
+        public Response(StatusCode statusCode)
+        {
+            this.StatusCode = statusCode;
+
+            this.Headers.Add("Server", "My Web Server");
+            this.Headers.Add("Date", $"{DateTime.UtcNow:r}");
+        }
+
+        public StatusCode StatusCode { get; init; }
+
+        public HeaderCollection Headers { get; } = new HeaderCollection();
+
+        public string Body { get; set; }
+    }
+}
