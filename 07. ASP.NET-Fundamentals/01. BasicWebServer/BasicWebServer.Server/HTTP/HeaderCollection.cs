62a13a630dbf24111d2ad42e3282c8d18553eba1
mikegsCoder <mikegsid@gmail.com>
2022-09-06 09:33:47 +0300

    Add HeaderCollection.cs

---
+++ 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Server/HTTP/HeaderCollection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace BasicWebServer.Server.HTTP
+{
+    public class HeaderCollection
+    {
+        private readonly Dictionary<string, Header> headers;
+
+        public HeaderCollection()
+            => this.headers = new Dictionary<string, Header>();
+
+        public int Count => this.headers.Count;
+
+        public void Add(string name, string value)
+        {
+            var header = new Header(name, value);
+
+            this.headers.Add(name, header);
+        }
+    }
+}
