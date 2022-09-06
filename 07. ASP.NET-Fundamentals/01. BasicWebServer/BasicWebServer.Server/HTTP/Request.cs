2220eb1f58e13a3b5c8ccf376185664f97d03a2e
mikegsCoder <mikegsid@gmail.com>
2022-09-06 09:37:42 +0300

    Add Request.cs

---
+++ 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Server/HTTP/Request.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace BasicWebServer.Server.HTTP
+{
+    public class Request
+    {
+        public Method Method { get; private set; }
+
+        public string Url { get; private set; }
+
+        public HeaderCollection Headers { get; private set; }
+
+        public string Body { get; private set; }
+
+        public static Request Parse(string request)
+        {
+            var lines = request.Split("\r\n");
+
+            var startLine = lines.First().Split(" ");
+
+            var method = ParseMethod(startLine[0]);
+            var url = startLine[1];
+
+            var headers = ParseHeaders(lines.Skip(1));
+
+            var bodyLines = lines.Skip(headers.Count + 2).ToArray();
+
+            var body = string.Join("\r\n", bodyLines);
+
+            return new Request
+            {
+                Method = method,
+                Url = url,
+                Headers = headers,
+                Body = body
+            };
+        }
+
+        private static Method ParseMethod(string method)
+        {
+            try
+            {
+                return (Method)Enum.Parse(typeof(Method), method, true);
+            }
+            catch (Exception)
+            {
+                throw new InvalidOperationException($"Method '{method}' is not supported");
+            }
+        }
+
+        private static HeaderCollection ParseHeaders(IEnumerable<string> headerLines)
+        {
+            var headerCollection = new HeaderCollection();
+
+            foreach (var headerLine in headerLines)
+            {
+                if (headerLine == string.Empty)
+                {
+                    break;
+                }
+
+                var headerParts = headerLine.Split(":", 2);
+
+                if (headerParts.Length != 2)
+                {
+                    throw new InvalidOperationException("Request is not valid.");
+                }
+
+                var headerName = headerParts[0];
+                var headerValue = headerParts[1].Trim();
+
+                headerCollection.Add(headerName, headerValue);
+            }
+
+            return headerCollection;
+        }
+    }
+}
