e82c4fb08ed3945605473cf1698924b735b965b0
mikegsCoder <mikegsid@gmail.com>
2022-09-06 09:32:06 +0300

    Add Header.cs

---
+++ 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Server/HTTP/Header.cs
@@ -0,0 +1,20 @@
+﻿using BasicWebServer.Server.Common;
+
+namespace BasicWebServer.Server.HTTP
+{
+    public class Header
+    {
+        public Header(string name, string value)
+        {
+            Guard.AgainstNull(name, nameof(name));
+            Guard.AgainstNull(value, nameof(value));
+
+            this.Name = name;
+            this.Value = value;
+        }
+
+        public string Name { get; init; }
+
+        public string Value { get; set; }
+    }
+}
