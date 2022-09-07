756f906dc7dd0ce19ab8910399388a6ae2a57144
mikegsCoder <mikegsid@gmail.com>
2022-09-07 15:51:57 +0300

    Modify Header

--- 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Server/HTTP/Header.cs
+++ 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Server/HTTP/Header.cs
@@ -4,6 +4,12 @@ namespace BasicWebServer.Server.HTTP
 {
     public class Header
     {
+        public const string ContentType = "Content-Type";
+        public const string ContentLength = "Content-Length";
+        public const string Date = "Date";
+        public const string Location = "Location";
+        public const string Server = "Server";
+
         public Header(string name, string value)
         {
             Guard.AgainstNull(name, nameof(name));
@@ -16,5 +22,8 @@ namespace BasicWebServer.Server.HTTP
         public string Name { get; init; }
 
         public string Value { get; set; }
+
+        public override string ToString()
+            => $"{this.Name}: {this.Value}";
     }
 }
