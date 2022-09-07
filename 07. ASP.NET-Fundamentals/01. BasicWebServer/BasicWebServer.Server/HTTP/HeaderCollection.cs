c3addeb36acdf1ad3ae1d04995170cd9573e8096
mikegsCoder <mikegsid@gmail.com>
2022-09-07 15:43:09 +0300

    Modify HeaderCollection

--- 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Server/HTTP/HeaderCollection.cs
+++ 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Server/HTTP/HeaderCollection.cs
@@ -1,21 +1,30 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 
 namespace BasicWebServer.Server.HTTP
 {
-    public class HeaderCollection
+    public class HeaderCollection : IEnumerable<Header>
     {
         private readonly Dictionary<string, Header> headers;
 
         public HeaderCollection()
             => this.headers = new Dictionary<string, Header>();
 
+        public string this[string name]
+          => this.headers[name].Value;
+
         public int Count => this.headers.Count;
 
-        public void Add(string name, string value)
-        {
-            var header = new Header(name, value);
+        public bool Contains(string name)
+          => this.headers.ContainsKey(name);
 
-            this.headers.Add(name, header);
-        }
+        public void Add(string name, string value)
+            => this.headers[name] = new Header(name, value);
+
+        public IEnumerator<Header> GetEnumerator()
+            => this.headers.Values.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => this.GetEnumerator();
     }
 }
