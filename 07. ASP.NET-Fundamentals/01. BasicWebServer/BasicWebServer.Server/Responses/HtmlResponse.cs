1785fbb163047106515b5a3c4120f7c96113f8df
mikegsCoder <mikegsid@gmail.com>
2022-09-08 16:22:18 +0300

    Add HtmlResponse

---
+++ 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Server/Responses/HtmlResponse.cs
@@ -0,0 +1,12 @@
+﻿using BasicWebServer.Server.HTTP;
+
+namespace BasicWebServer.Server.Responses
+{
+    public class HtmlResponse : ContentResponse
+    {
+        public HtmlResponse(string text)
+            : base(text, ContentType.Html)
+        {
+        }
+    }
+}
