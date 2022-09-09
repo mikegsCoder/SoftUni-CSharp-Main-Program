f78a3a2b587f6f9b9bae2185c8e5c4ec7ffdf870
mikegsCoder <mikegsid@gmail.com>
2022-09-09 15:50:23 +0300

    Add HtmlForm

--- 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Demo/Startup.cs
+++ 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Demo/Startup.cs
@@ -4,6 +4,12 @@ namespace BasicWebServer.Demo
 {
     public class Startup
     {
+        private const string HtmlForm = @"<form action='/HTML' method='POST'>
+            Name: <input type='text' name='Name'/>
+            Age: <input type='number' name ='Age'/>
+            <input type='submit' value ='Save' />
+        </form>";
+
         public static void Main()
         {
             var server = new HttpServer("127.0.0.1", 8080);
