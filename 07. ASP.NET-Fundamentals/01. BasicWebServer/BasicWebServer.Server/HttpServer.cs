0f6e594a9003ac50919a3e698a6ae03607f7d124
mikegsCoder <mikegsid@gmail.com>
2022-09-09 16:05:20 +0300

    Modify Start method

--- 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Server/HttpServer.cs
+++ 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Server/HttpServer.cs
@@ -56,7 +56,10 @@ namespace BasicWebServer.Server
 
                 var response = this.routingTable.MatchRequest(request);
 
-                
+                // Execute pre-render action for the response
+                if (response.PreRenderAction != null)
+                    response.PreRenderAction(request, response);
+
                 WriteResponse(networkStream, response);
 
                 connection.Close();
