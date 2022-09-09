2a1cfaa21c18bf8deddf1c1c30ced02de2d566a2
mikegsCoder <mikegsid@gmail.com>
2022-09-09 15:42:22 +0300

    Modify HttpServer

--- 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Server/HttpServer.cs
+++ 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Server/HttpServer.cs
@@ -2,6 +2,8 @@
 using System.Net;
 using System.Text;
 using System.Net.Sockets;
+using BasicWebServer.Server.HTTP;
+using BasicWebServer.Server.Routing;
 
 namespace BasicWebServer.Server
 {
@@ -11,12 +13,26 @@ namespace BasicWebServer.Server
         private readonly int port;
         private readonly TcpListener serverListener;
 
-        public HttpServer(string ipAddress, int port)
+        private readonly RoutingTable routingTable;
+
+        public HttpServer(string ipAddress, int port, Action<IRoutingTable> routingTableConfiguration)
         {
             this.ipAddress = IPAddress.Parse(ipAddress);
             this.port = port;
 
             this.serverListener = new TcpListener(this.ipAddress, port);
+
+            routingTableConfiguration(this.routingTable = new RoutingTable());
+        }
+
+        public HttpServer(int port, Action<IRoutingTable> routingTable)
+            : this("127.0.0.1", port, routingTable)
+        {
+        }
+
+        public HttpServer(Action<IRoutingTable> routingTable)
+            : this(8080, routingTable)
+        {
         }
 
         public void Start()
@@ -36,7 +52,12 @@ namespace BasicWebServer.Server
 
                 Console.WriteLine(requestText);
 
-                WriteResponse(networkStream, "Hello from the server!");
+                var request = Request.Parse(requestText);
+
+                var response = this.routingTable.MatchRequest(request);
+
+                
+                WriteResponse(networkStream, response);
 
                 connection.Close();
             }
@@ -70,17 +91,9 @@ namespace BasicWebServer.Server
             return requestBuilder.ToString();
         }
 
-        private void WriteResponse(NetworkStream networkStream, string message)
+        private void WriteResponse(NetworkStream networkStream, Response response)
         {
-            var contentLength = Encoding.UTF8.GetByteCount(message);
-
-            var response = $@"HTTP/1.1 200 OK
-Content-Type: text/plain; charset=UTF-8
-Content-Length: {contentLength}
-
-{message}";
-
-            var resposeBytes = Encoding.UTF8.GetBytes(response);
+            var resposeBytes = Encoding.UTF8.GetBytes(response.ToString());
 
             networkStream.Write(resposeBytes);
         }
