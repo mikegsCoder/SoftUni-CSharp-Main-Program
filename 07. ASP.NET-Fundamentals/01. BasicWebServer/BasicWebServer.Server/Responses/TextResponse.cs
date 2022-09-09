2254a59163f3734a960b2f98a5c732bb3e0ac49c
mikegsCoder <mikegsid@gmail.com>
2022-09-09 16:02:39 +0300

    Modify ContentResponse

--- 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Server/Responses/TextResponse.cs
+++ 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Server/Responses/TextResponse.cs
@@ -5,7 +5,7 @@ namespace BasicWebServer.Server.Responses
 {
     public class TextResponse : ContentResponse
     {
-        public TextResponse(string text, 
+        public TextResponse(string text,
             Action<Request, Response> preRenderAction = null)
             : base(text, ContentType.PlainText, preRenderAction)
         {
