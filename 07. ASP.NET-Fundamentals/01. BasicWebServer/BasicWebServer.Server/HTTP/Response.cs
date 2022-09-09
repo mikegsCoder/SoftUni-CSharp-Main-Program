63adbb8d89041c18dcbb81126b652776bb9ceda9
mikegsCoder <mikegsid@gmail.com>
2022-09-09 15:45:27 +0300

    Override ToString method in Response

--- 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Server/HTTP/Response.cs
+++ 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Server/HTTP/Response.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace BasicWebServer.Server.HTTP
 {
@@ -17,5 +18,26 @@ namespace BasicWebServer.Server.HTTP
         public HeaderCollection Headers { get; } = new HeaderCollection();
 
         public string Body { get; set; }
+
+        public override string ToString()
+        {
+            var result = new StringBuilder();
+
+            result.AppendLine($"HTTP/1.1 {(int)this.StatusCode} {this.StatusCode}");
+
+            foreach (var header in this.Headers)
+            {
+                result.AppendLine(header.ToString());
+            }
+
+            result.AppendLine();
+
+            if (!string.IsNullOrEmpty(this.Body))
+            {
+                result.Append(this.Body);
+            }
+
+            return result.ToString();
+        }
     }
 }
