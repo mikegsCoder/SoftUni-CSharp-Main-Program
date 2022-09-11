7f3ea837075cd815cb9fe8067b870bbf225f6bfe
mikegsCoder <mikegsid@gmail.com>
2022-09-11 10:28:24 +0300

    Add cookies in Response

--- 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Server/HTTP/Response.cs
+++ 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Server/HTTP/Response.cs
@@ -17,6 +17,8 @@ namespace BasicWebServer.Server.HTTP
 
         public HeaderCollection Headers { get; } = new HeaderCollection();
 
+        public CookieCollection Cookies { get; } = new CookieCollection();
+
         public string Body { get; set; }
 
         public Action<Request, Response> PreRenderAction { get; protected set; }
@@ -30,6 +32,11 @@ namespace BasicWebServer.Server.HTTP
             foreach (var header in this.Headers)
             {
                 result.AppendLine(header.ToString());
+            }
+
+            foreach (var cookie in this.Cookies)
+            {
+                result.AppendLine($"{Header.SetCookie}: {cookie}");
             }
 
             result.AppendLine();
