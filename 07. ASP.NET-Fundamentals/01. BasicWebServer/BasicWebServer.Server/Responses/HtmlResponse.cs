c813aea7683812a54505548873efe4ffb23f2c51
mikegsCoder <mikegsid@gmail.com>
2022-09-12 17:10:00 +0300

    Modify HtmlResponse

--- 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Server/Responses/HtmlResponse.cs
+++ 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Server/Responses/HtmlResponse.cs
@@ -1,11 +1,13 @@
 ﻿using BasicWebServer.Server.HTTP;
+using System;
 
 namespace BasicWebServer.Server.Responses
 {
     public class HtmlResponse : ContentResponse
     {
-        public HtmlResponse(string text)
-            : base(text, ContentType.Html)
+        public HtmlResponse(string text,
+            Action<Request, Response> preRenderAction = null)
+            : base(text, ContentType.Html, preRenderAction)
         {
         }
     }
