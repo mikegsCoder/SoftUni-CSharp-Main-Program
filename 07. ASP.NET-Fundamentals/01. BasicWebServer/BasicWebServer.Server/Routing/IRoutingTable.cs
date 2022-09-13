c43fc823e14a48871824210a838b5cb43ac8c47e
mikegsCoder <mikegsid@gmail.com>
2022-09-13 15:19:17 +0300

    Modify IRoutingTable

--- 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Server/Routing/IRoutingTable.cs
+++ 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Server/Routing/IRoutingTable.cs
@@ -1,11 +1,14 @@
 ﻿using BasicWebServer.Server.HTTP;
+using System;
 
 namespace BasicWebServer.Server.Routing
 {
     public interface IRoutingTable
     {
-        IRoutingTable Map(string url, Method method, Response response);
-        IRoutingTable MapGet(string url, Response response);
-        IRoutingTable MapPost(string url, Response response);
+        IRoutingTable Map(Method method, string path, Func<Request, Response> responseFunction);
+
+        IRoutingTable MapGet(string path, Func<Request, Response> responseFunction);
+
+        IRoutingTable MapPost(string path, Func<Request, Response> responseFunction);
     }
 }
