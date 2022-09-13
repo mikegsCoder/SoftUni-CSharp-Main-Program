4c51cd60dc5f3eb5810a292391a2de0929de978e
mikegsCoder <mikegsid@gmail.com>
2022-09-13 15:07:23 +0300

    Modify Startup to work with Session

--- 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Demo/Startup.cs
+++ 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Demo/Startup.cs
@@ -27,6 +27,16 @@ namespace BasicWebServer.Demo
 
         private const string FileName = "content.txt";
 
+        private const string LoginForm = @"<form action='/Login' method='POST'>
+                Username: <input type='text' name='Username'/>
+                Password: <input type='text' name='Password'/>
+                <input type='submit' value ='Log In' /> 
+            </form>";
+
+        private const string Username = "user";
+
+        private const string Password = "user123";
+
         public static async Task Main()
         {
             await DownloadSitesAsTextFile(Startup.FileName,
@@ -39,7 +49,12 @@ namespace BasicWebServer.Demo
                .MapPost("/HTML", new TextResponse("", Startup.AddFormDataAction))
                .MapGet("/Content", new HtmlResponse(Startup.DownloadForm))
                .MapPost("/Content", new TextFileResponse(Startup.FileName))
-               .MapGet("/Cookies", new HtmlResponse("", Startup.AddCookiesAction)));
+               .MapGet("/Cookies", new HtmlResponse("", Startup.AddCookiesAction))
+               .MapGet("/Session", new TextResponse("", Startup.DisplaySessionInfoAction))
+               .MapGet("/Login", new HtmlResponse(Startup.LoginForm))
+               .MapPost("/Login", new HtmlResponse("", Startup.LoginAction))
+               .MapGet("/Logout", new HtmlResponse("", Startup.LogoutAction))
+               .MapGet("/UserProfile", new HtmlResponse("", Startup.GetUserDataAction)));
 
             await server.Start();
         }
@@ -124,6 +139,75 @@ namespace BasicWebServer.Demo
             {
                 response.Cookies.Add("My-Cookie", "My-Value");
                 response.Cookies.Add("My-Second-Cookie", "My-Second-Value");
+            }
+        }
+
+        private static void DisplaySessionInfoAction(Request request, Response response)
+        {
+            var sessionExists = request.Session
+                .ContainsKey(Session.SessionCurrentDateKey);
+
+            var bodyText = "";
+
+            if (sessionExists)
+            {
+                var currentDate = request.Session[Session.SessionCurrentDateKey];
+                bodyText = $"Stored date: {currentDate}!";
+            }
+            else
+            {
+                bodyText = "Current date stored!";
+            }
+
+            response.Body = "";
+            response.Body += bodyText;
+        }
+
+        private static void LoginAction(Request request, Response response)
+        {
+            request.Session.Clear();
+
+            var bodyText = "";
+
+            var usernameMatches = request.Form["Username"] == Startup.Username;
+            var passwordMatches = request.Form["Password"] == Startup.Password;
+
+            if (usernameMatches && passwordMatches)
+            {
+                request.Session[Session.SessionUserKey] = "MyUserId";
+                response.Cookies.Add(Session.SessionCookieName,
+                    request.Session.Id);
+
+                bodyText = "<h3>Logged successfully!</h3>";
+            }
+            else
+            {
+                bodyText = Startup.LoginForm;
+            }
+
+            response.Body = "";
+            response.Body += bodyText;
+        }
+
+        private static void LogoutAction(Request request, Response response)
+        {
+            request.Session.Clear();
+
+            response.Body = "";
+            response.Body += "<h3>Logged out successfully!</h3>";
+        }
+
+        private static void GetUserDataAction(Request request, Response response)
+        {
+            if (request.Session.ContainsKey(Session.SessionUserKey))
+            {
+                response.Body = "";
+                response.Body += $"<h3>Currently logged-in user is with username '{Username}'</h3>";
+            }
+            else
+            {
+                response.Body = "";
+                response.Body += "<h3>You should first log in - <a href='/Login'>Login</a></h3>";
             }
         }
     }
