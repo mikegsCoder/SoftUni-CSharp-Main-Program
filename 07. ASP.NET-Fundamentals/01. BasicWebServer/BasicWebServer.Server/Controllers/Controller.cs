d1cb745b0a2575773653edcd8c953ba0a9637e39
mikegsCoder <mikegsid@gmail.com>
2022-09-13 15:26:31 +0300

    Add Controller class

---
+++ 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Server/Controllers/Controller.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.CompilerServices;
+using BasicWebServer.Server.HTTP;
+using BasicWebServer.Server.Responses;
+
+namespace BasicWebServer.Server.Controllers
+{
+    public abstract class Controller
+    {
+        protected Controller(Request request)
+        {
+            this.Request = request;
+        }
+        
+        protected Request Request { get; private init; }
+
+        protected Response Text(string text) => new TextResponse(text);
+
+        protected Response Html(string text) => new HtmlResponse(text);
+
+        protected Response BadRequest() => new BadRequestResponse();
+
+        protected Response Unauthorized() => new UnauthorizedResponse();
+
+        protected Response NotFound() => new NotFoundResponse();
+
+        protected Response Redirect(string location) => new RedirectResponse(location);
+
+        protected Response File(string fileName) => new TextFileResponse(fileName);
+    }
+}
