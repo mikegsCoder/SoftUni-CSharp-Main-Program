ae4184530d6563678ea88c728904aa0bf528ffad
mikegsCoder <mikegsid@gmail.com>
2022-09-14 19:27:56 +0300

    Add HomeController

---
+++ 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Demo/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using BasicWebServer.Server.Controllers;
+using BasicWebServer.Server.HTTP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicWebServer.Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController(Request request) : base(request)
+        {
+        }
+    }
+}
