bf6ba4a3638e344e7f67d95bf08c3be85b40c839
mikegsCoder <mikegsid@gmail.com>
2022-09-14 20:15:39 +0300

    Modify StartUp

--- 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Demo/Startup.cs
+++ 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Demo/Startup.cs
@@ -17,16 +17,6 @@ namespace BasicWebServer.Demo
 {
     public class Startup
     {
-        private const string LoginForm = @"<form action='/Login' method='POST'>
-                Username: <input type='text' name='Username'/>
-                Password: <input type='text' name='Password'/>
-                <input type='submit' value ='Log In' /> 
-            </form>";
-
-        private const string Username = "user";
-
-        private const string Password = "user123";
-
         public static async Task Main()
             => await new HttpServer(routes => routes
                 .MapGet<HomeController>("/", c => c.Index())
@@ -34,130 +24,9 @@ namespace BasicWebServer.Demo
                 .MapGet<HomeController>("/HTML", c => c.Html())
                 .MapPost<HomeController>("/HTML", c => c.HtmlFormPost())
                 .MapGet<HomeController>("/Content", c => c.Content())
-                .MapPost<HomeController>("/Content", c => c.DownloadContent()))
+                .MapPost<HomeController>("/Content", c => c.DownloadContent())
+                .MapGet<HomeController>("/Cookies", c => c.Cookies())
+                .MapGet<HomeController>("/Session", c => c.Session()))
             .Start();
-
-        private static void AddFormDataAction(Request request, Response response)
-        {
-            response.Body = "";
-
-            foreach (var (key, value) in request.Form)
-            {
-                response.Body += $"{key} - {value}";
-                response.Body += Environment.NewLine;
-            }
-        }
-
-
-
-        private static void AddCookiesAction(Request request, Response response)
-        {
-            var requestHasCookies = request.Cookies.Any(c => c.Name != Session.SessionCookieName);
-            var bodyText = "";
-
-            if (requestHasCookies)
-            {
-                var cookieText = new StringBuilder();
-                cookieText.AppendLine("<h1>Cookies</h1>");
-
-                cookieText
-                    .Append("<table border='1'><tr><th>Name</th><th>Value</th></tr>");
-
-                foreach (var cookie in request.Cookies)
-                {
-                    cookieText.Append("<tr>");
-                    cookieText
-                        .Append($"<td>{HttpUtility.HtmlEncode(cookie.Name)}</td>");
-                    cookieText
-                        .Append($"<td>{HttpUtility.HtmlEncode(cookie.Value)}</td>");
-                    cookieText.Append("</tr>");
-                }
-                cookieText.Append("</table>");
-
-                bodyText = cookieText.ToString();
-            }
-            else
-            {
-                bodyText = "<h1>Cookies set!</h1>";
-            }
-
-            response.Body = "";
-            response.Body += bodyText;
-
-            if (!requestHasCookies)
-            {
-                response.Cookies.Add("My-Cookie", "My-Value");
-                response.Cookies.Add("My-Second-Cookie", "My-Second-Value");
-            }
-        }
-
-        private static void DisplaySessionInfoAction(Request request, Response response)
-        {
-            var sessionExists = request.Session
-                .ContainsKey(Session.SessionCurrentDateKey);
-
-            var bodyText = "";
-
-            if (sessionExists)
-            {
-                var currentDate = request.Session[Session.SessionCurrentDateKey];
-                bodyText = $"Stored date: {currentDate}!";
-            }
-            else
-            {
-                bodyText = "Current date stored!";
-            }
-
-            response.Body = "";
-            response.Body += bodyText;
-        }
-
-        private static void LoginAction(Request request, Response response)
-        {
-            request.Session.Clear();
-
-            var bodyText = "";
-
-            var usernameMatches = request.Form["Username"] == Startup.Username;
-            var passwordMatches = request.Form["Password"] == Startup.Password;
-
-            if (usernameMatches && passwordMatches)
-            {
-                request.Session[Session.SessionUserKey] = "MyUserId";
-                response.Cookies.Add(Session.SessionCookieName,
-                    request.Session.Id);
-
-                bodyText = "<h3>Logged successfully!</h3>";
-            }
-            else
-            {
-                bodyText = Startup.LoginForm;
-            }
-
-            response.Body = "";
-            response.Body += bodyText;
-        }
-
-        private static void LogoutAction(Request request, Response response)
-        {
-            request.Session.Clear();
-
-            response.Body = "";
-            response.Body += "<h3>Logged out successfully!</h3>";
-        }
-
-        private static void GetUserDataAction(Request request, Response response)
-        {
-            if (request.Session.ContainsKey(Session.SessionUserKey))
-            {
-                response.Body = "";
-                response.Body += $"<h3>Currently logged-in user is with username '{Username}'</h3>";
-            }
-            else
-            {
-                response.Body = "";
-                response.Body += "<h3>You should first log in - <a href='/Login'>Login</a></h3>";
-            }
-        }
     }
 }
