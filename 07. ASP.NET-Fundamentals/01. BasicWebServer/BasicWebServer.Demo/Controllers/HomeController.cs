ea785a8f5e6208b37eea03f15e58a6697a4403c0
mikegsCoder <mikegsid@gmail.com>
2022-09-14 19:36:27 +0300

    Update HomeController

--- 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Demo/Controllers/HomeController.cs
+++ 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Demo/Controllers/HomeController.cs
@@ -10,8 +10,20 @@ namespace BasicWebServer.Demo.Controllers
 {
     public class HomeController : Controller
     {
+        private const string HtmlForm = @"<form action='/HTML' method='POST'>
+            Name: <input type='text' name='Name'/>
+            Age: <input type='number' name ='Age'/>
+            <input type='submit' value ='Save' />
+        </form>";
+
         public HomeController(Request request) : base(request)
         {
         }
+
+        public Response Index() => Text("Hello from the server!");
+
+        public Response Redirect() => Redirect("https://softuni.org/");
+
+        public Response Html() => Html(HomeController.HtmlForm);
     }
 }
