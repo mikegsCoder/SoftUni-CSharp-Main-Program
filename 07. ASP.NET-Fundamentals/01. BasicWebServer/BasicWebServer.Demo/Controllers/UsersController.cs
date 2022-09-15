a4386dfb59779c97178aeb9345845b6258f82751
mikegsCoder <mikegsid@gmail.com>
2022-09-15 19:06:41 +0300

    Add Login method in UserController

--- 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Demo/Controllers/UsersController.cs
+++ 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Demo/Controllers/UsersController.cs
@@ -5,9 +5,17 @@ namespace BasicWebServer.Demo.Controllers
 {
     public class UsersController : Controller
     {
+        private const string LoginForm = @"<form action='/Login' method='POST'>
+                Username: <input type='text' name='Username'/>
+                Password: <input type='text' name='Password'/>
+                <input type='submit' value ='Log In' /> 
+            </form>";
+
         public UsersController(Request request) 
             : base(request)
         {
         }
+
+        public Response Login() => Html(UsersController.LoginForm);
     }
 }
