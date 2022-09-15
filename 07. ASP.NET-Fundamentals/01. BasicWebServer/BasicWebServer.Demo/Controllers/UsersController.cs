625bca13e3e91907595e40946691ad76cf31fb07
mikegsCoder <mikegsid@gmail.com>
2022-09-15 19:11:23 +0300

    Add LogInUser method in UserController

--- 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Demo/Controllers/UsersController.cs
+++ 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Demo/Controllers/UsersController.cs
@@ -11,11 +11,41 @@ namespace BasicWebServer.Demo.Controllers
                 <input type='submit' value ='Log In' /> 
             </form>";
 
+        private const string Username = "user";
+
+        private const string Password = "user123";
+
         public UsersController(Request request) 
             : base(request)
         {
         }
 
         public Response Login() => Html(UsersController.LoginForm);
+
+        public Response LogInUser()
+        {
+            this.Request.Session.Clear();
+
+            var usernameMatches = this.Request.Form["Username"] == UsersController.Username;
+            var passwordMatches = this.Request.Form["Password"] == UsersController.Password;
+
+            if (usernameMatches && passwordMatches)
+            {
+                if (!this.Request.Session.ContainsKey(Session.SessionUserKey))
+                {
+                    this.Request.Session[Session.SessionUserKey] = "MyUserId";
+
+                    var cookies = new CookieCollection();
+                    cookies.Add(Session.SessionCookieName,
+                        this.Request.Session.Id);
+
+                    return Html("<h3>Logged successfully!</h3>", cookies);
+                }
+
+                return Html("<h3>Logged successfully!</h3>");
+            }
+
+            return Redirect("/Login");
+        }
     }
 }
