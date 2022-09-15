4ec0a1c7ef9d94522464ffba03b6a723e5d4e91c
mikegsCoder <mikegsid@gmail.com>
2022-09-15 19:04:32 +0300

    Add UserConroller

---
+++ 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Demo/Controllers/UsersController.cs
@@ -0,0 +1,13 @@
+﻿using BasicWebServer.Server.HTTP;
+using BasicWebServer.Server.Controllers;
+
+namespace BasicWebServer.Demo.Controllers
+{
+    public class UsersController : Controller
+    {
+        public UsersController(Request request) 
+            : base(request)
+        {
+        }
+    }
+}
