c076427474119681aa0fc713894356d74d0d09a9
mikegsCoder <mikegsid@gmail.com>
2022-09-16 16:32:22 +0300

    Remove pre-render action from TextResponse

--- 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Server/Responses/TextResponse.cs
+++ 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Server/Responses/TextResponse.cs
@@ -5,9 +5,8 @@ namespace BasicWebServer.Server.Responses
 {
     public class TextResponse : ContentResponse
     {
-        public TextResponse(string text,
-            Action<Request, Response> preRenderAction = null)
-            : base(text, ContentType.PlainText, preRenderAction)
+        public TextResponse(string text)
+            : base(text, ContentType.PlainText)
         {
         }
     }
