5e2528e42d6fbc3de9c6da9f0515424575c1096f
mikegsCoder <mikegsid@gmail.com>
2022-09-17 11:13:46 +0300

    Modify Controller to retrn View

--- 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Server/Controllers/Controller.cs
+++ 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Server/Controllers/Controller.cs
@@ -39,5 +39,12 @@ namespace BasicWebServer.Server.Controllers
         protected Response Redirect(string location) => new RedirectResponse(location);
 
         protected Response File(string fileName) => new TextFileResponse(fileName);
+
+        protected Response View([CallerMemberName] string viewName = "")
+           => new ViewResponse(viewName, this.GetControllerName());
+
+        private string GetControllerName()
+            => this.GetType().Name
+                .Replace(nameof(Controller), string.Empty);
     }
 }
