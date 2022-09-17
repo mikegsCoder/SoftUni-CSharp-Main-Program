17bb6da57cfe4be08a805253287863d38000c447
mikegsCoder <mikegsid@gmail.com>
2022-09-17 11:11:11 +0300

    Add ViewResponse class

---
+++ 07. ASP.NET-Fundamentals/01. BasicWebServer/BasicWebServer.Server/Responses/ViewResponse.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using BasicWebServer.Server.HTTP;
+
+namespace BasicWebServer.Server.Responses
+{
+    public class ViewResponse : ContentResponse
+    {
+        private const char PathSeparator = '/';
+
+        public ViewResponse(string viewName, string controllerName) 
+            : base("", ContentType.Html)
+        {
+            if (!viewName.Contains(PathSeparator))
+            {
+                viewName = controllerName + PathSeparator + viewName;
+            }
+
+            var viewPath = Path.GetFullPath(
+                $"./Views/" +
+                viewName.TrimStart(PathSeparator)
+                + ".cshtml");
+
+            var viewContent = File.ReadAllText(viewPath);
+
+            this.Body = viewContent;
+        }
+    }
+}
