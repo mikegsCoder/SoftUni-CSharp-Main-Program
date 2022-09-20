7e68742e5198f8a54985fc9106c80ad1faad005b
mikegsCoder <mikegsid@gmail.com>
2022-09-20 16:42:58 +0300

    Add IChoronometer interface

---
+++ 07. ASP.NET-Fundamentals/02. Chronometer/ChronometerApp/IChronometer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace ChronometerApp
+{
+    public interface IChronometer
+    {
+        string GetTime { get; }
+        List<string> Laps { get; }
+        void Start();
+        void Stop();
+        string Lap();
+        void Reset();
+    }
+}
