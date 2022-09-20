f6fa75801d8da5bbe990d56aa126a41ffa8bef40
mikegsCoder <mikegsid@gmail.com>
2022-09-20 16:45:24 +0300

    Add Chronometer class

---
+++ 07. ASP.NET-Fundamentals/02. Chronometer/ChronometerApp/Chronometer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChronometerApp
+{
+    internal class Chronometer : IChronometer
+    {
+        public string GetTime => throw new NotImplementedException();
+
+        public List<string> Laps => throw new NotImplementedException();
+
+        public string Lap()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
