747229510fc242b28fa0a7311a04df8e4b1daad0
mikegsCoder <mikegsid@gmail.com>
2022-09-23 10:56:11 +0300

    Add async method ReadData

--- 07. ASP.NET-Fundamentals/03. HttpClientDemo/HttpClientDemo/Program.cs
+++ 07. ASP.NET-Fundamentals/03. HttpClientDemo/HttpClientDemo/Program.cs
@@ -6,5 +6,17 @@
         {
             
         }
+
+        public static async Task ReadData()
+        {
+            string url = "https://softuni.bg/courses/csharp-web-basics";
+            HttpClient httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(url);
+            Console.WriteLine(response.StatusCode);
+            Console.WriteLine(string.Join(Environment.NewLine, response.Headers.Select(x => x.Key + ": " + x.Value.First())));
+
+            // var html = await httpClient.GetStringAsync(url);
+            // Console.WriteLine(html);
+        }
     }
 }
