b0b6f7a41d9a500392b15c163882348a59d42ea5
mikegsCoder <mikegsid@gmail.com>
2022-09-23 11:11:14 +0300

    Add main functionality

--- 07. ASP.NET-Fundamentals/03. HttpClientDemo/HttpClientDemo/Program.cs
+++ 07. ASP.NET-Fundamentals/03. HttpClientDemo/HttpClientDemo/Program.cs
@@ -13,6 +13,36 @@ namespace HttpClientDemo
 
             TcpListener tcpListener = new TcpListener(IPAddress.Loopback, 80);  // => choice a port to listen
             tcpListener.Start();  // => start the listener
+            while (true)  // => until the request is correct
+            {
+                var client = tcpListener.AcceptTcpClient(); // => add client
+                using (var stream = client.GetStream())  // => open stream
+                {
+                    byte[] buffer = new byte[1000000]; // => add new byte[]
+                    var lenght = stream.Read(buffer, 0, buffer.Length); // => calculate the byte[] length
+
+                    string requestString = Encoding.UTF8.GetString(buffer, 0, lenght);  // => transform the byte[] into string
+                    Console.WriteLine(requestString);
+                    string html = $"<h1>Hello from Mihail_Server {DateTime.Now}</h1>" +
+                        $"<form action=/tweet method=post><input name=username /><input name=password />" +
+                        $"<input type=submit /></form>";
+                    string response = "HTTP/1.1 200 OK" // => create reponse
+                        + NewLine + "Server: MihailServer 2022"
+                        + NewLine +
+                        //"Location: https://www.google.com" + NewLine +
+                        "Content-Type: text/html; charset=utf-8"
+                        + NewLine +
+                        // "Content-Disposition: attachment; filename=mike.txt" + NewLine +
+                        "Content-Lenght: " + html.Length
+                        + NewLine
+                        + NewLine + html
+                        + NewLine;
+
+                    byte[] responseBytes = Encoding.UTF8.GetBytes(response); // => transform the response into byte[]
+                    stream.Write(responseBytes);
+                    Console.WriteLine(new string('=', 70));
+                }
+            }
         }
 
         public static async Task ReadData()
