fbb8d4b465118ed20cdc35e853e2d408b8f4b5b1
mikegsCoder <mikegsid@gmail.com>
2022-09-24 12:25:17 +0300

    Cleanup and code formatting

--- 07. ASP.NET-Fundamentals/04. AsynchronousProgrammingDemo/AsynchronousProgrammingDemo/Program.cs
+++ 07. ASP.NET-Fundamentals/04. AsynchronousProgrammingDemo/AsynchronousProgrammingDemo/Program.cs
@@ -4,13 +4,14 @@
     {
         static async Task Main(string[] args)
         {
-
             HttpClient httpClient = new HttpClient();
             string url = "https://softuni.bg";
+
             HttpResponseMessage httpResponse = await httpClient.GetAsync(url);
             string result = await httpResponse.Content.ReadAsStringAsync();
+
             Console.WriteLine(result);
-            Console.WriteLine("Control string");
+            Console.WriteLine("Control string"); // this Control string could be printed before the result!
         }
     }
 }
