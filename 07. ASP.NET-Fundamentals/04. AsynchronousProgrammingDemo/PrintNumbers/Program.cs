0c130c033e8c3a3f5f6dec34bb0bff919d58e5ad
mikegsCoder <mikegsid@gmail.com>
2022-09-25 12:42:08 +0300

    Add PrintUnevenNumbers method

--- 07. ASP.NET-Fundamentals/04. AsynchronousProgrammingDemo/PrintNumbers/Program.cs
+++ 07. ASP.NET-Fundamentals/04. AsynchronousProgrammingDemo/PrintNumbers/Program.cs
@@ -19,5 +19,18 @@
             }
             Console.WriteLine("Thread EVEN finished work");
         }
+
+        private static void PrintUnevenNumbers()
+        {
+
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine("Thread UNEVEN finished work");
+        }
     }
 }
