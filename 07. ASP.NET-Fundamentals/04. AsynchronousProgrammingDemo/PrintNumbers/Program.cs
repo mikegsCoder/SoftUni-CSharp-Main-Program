3c97eceb13d59bbdef0b0a84556339060d82594e
mikegsCoder <mikegsid@gmail.com>
2022-09-25 12:40:30 +0300

    Add PrintEvenNumbers method

--- 07. ASP.NET-Fundamentals/04. AsynchronousProgrammingDemo/PrintNumbers/Program.cs
+++ 07. ASP.NET-Fundamentals/04. AsynchronousProgrammingDemo/PrintNumbers/Program.cs
@@ -6,5 +6,18 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
+        private static void PrintEvenNumbers()
+        {
+
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine("Thread EVEN finished work");
+        }
     }
 }
