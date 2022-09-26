e7ac3419353a7b7c5945e005fbe72f0b9d32b5ad
mikegsCoder <mikegsid@gmail.com>
2022-09-26 16:19:28 +0300

    Code formatting

--- 07. ASP.NET-Fundamentals/04. AsynchronousProgrammingDemo/DataParallelism/Program.cs
+++ 07. ASP.NET-Fundamentals/04. AsynchronousProgrammingDemo/DataParallelism/Program.cs
@@ -6,12 +6,10 @@
         {
             List<int> elements = new List<int>() { 1, 2, 3 };
 
-
             Parallel.For(0, elements.Count, i =>
             {
                 Console.WriteLine(elements[i]);
             });
-
         }
     }
 }
