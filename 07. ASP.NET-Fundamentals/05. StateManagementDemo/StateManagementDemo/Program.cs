9bf30dafe8b62c0a3c66686e3493b3c9543588e7
mikegsCoder <mikegsid@gmail.com>
2022-09-27 16:10:26 +0300

    Add functionality in Main method

--- 07. ASP.NET-Fundamentals/05. StateManagementDemo/StateManagementDemo/Program.cs
+++ 07. ASP.NET-Fundamentals/05. StateManagementDemo/StateManagementDemo/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace StateManagementDemo
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Demo_State_Management
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        static Dictionary<string, int> SessionStorage = new Dictionary<string, int>();
+        
+
+        const string NewLine = "\r\n";
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = Encoding.UTF8;
+            TcpListener tcpListener = new TcpListener(IPAddress.Loopback, 80);
+            tcpListener.Start();
+
+            while (true)
+            {
+                TcpClient client = await tcpListener.AcceptTcpClientAsync();
+            }
+
         }
     }
 }
