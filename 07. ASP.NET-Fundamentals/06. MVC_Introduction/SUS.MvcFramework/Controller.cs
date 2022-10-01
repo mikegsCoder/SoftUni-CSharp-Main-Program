29f01a8d501578bf1066e01b02f23e97148618e7
mikegsCoder <mikegsid@gmail.com>
2022-10-01 12:10:47 +0300

    Add Controller class with View method

---
+++ 07. ASP.NET-Fundamentals/06. MVC_Introduction/SUS.MvcFramework/Controller.cs
@@ -0,0 +1,27 @@
+﻿using SUS.HTTP;
+using System.Text;
+using SUS.HTTP.Enums;
+using System.Runtime.CompilerServices;
+
+namespace SUS.MvcFramework
+{
+    public abstract class Controller
+    {
+        public HttpResponse View([CallerMemberName] string viewPath = null)
+        {
+            var layout = System.IO.File.ReadAllText("Views/Shared/_Layout.cshtml");
+
+            var viewContent = System.IO.File.ReadAllText($"Views/{this.GetType().Name.Replace("Controller", string.Empty)}/{viewPath}.cshtml");
+
+            var responseHtml = layout.Replace("@RenderBody()", viewContent);
+
+            var responseBodyBytes = Encoding.UTF8.GetBytes(responseHtml);
+
+            var response = new HttpResponse("text/html", responseBodyBytes);
+
+            return response;
+        }
+
+       
+    }
+}
