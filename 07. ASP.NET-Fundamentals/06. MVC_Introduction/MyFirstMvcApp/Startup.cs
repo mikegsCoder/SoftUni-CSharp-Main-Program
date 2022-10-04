71dee0cbfc0ce08aba3d1dab82ec8e1872a6a36a
mikegsCoder <mikegsid@gmail.com>
2022-10-04 16:55:01 +0300

    Add Startup class

---
+++ 07. ASP.NET-Fundamentals/06. MVC_Introduction/MyFirstMvcApp/Startup.cs
@@ -0,0 +1,26 @@
+﻿using SUS.HTTP;
+using SUS.MvcFramework;
+using MyFirstMvcApp.Controllers;
+using System.Collections.Generic;
+using SUS.HTTP.Enums;
+using HttpMethod = SUS.HTTP.Enums.HttpMethod;
+
+namespace MyFirstMvcApp
+{
+    public class Startup : IMvcApplication
+    {
+        public void ConfigureServices()
+        {
+            //throw new System.NotImplementedException();
+        }
+
+        public void Configure(List<Route> routeTable)
+        {
+            routeTable.Add(new Route("/", HttpMethod.Get, new HomeController().Index));
+
+            routeTable.Add(new Route("/users/login", HttpMethod.Get, new UsersController().Login));
+            routeTable.Add(new Route("/users/register", HttpMethod.Get, new UsersController().Register));
+            routeTable.Add(new Route("/users/login", HttpMethod.Post, new UsersController().DoLogin));
+        }
+    }
+}
