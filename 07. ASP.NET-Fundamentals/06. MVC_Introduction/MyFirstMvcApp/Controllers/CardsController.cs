c6d026df4f7fa963b7409d0e3ee42ec947c6cf3f
mikegsCoder <mikegsid@gmail.com>
2022-10-05 16:27:11 +0300

    Add CardsController

---
+++ 07. ASP.NET-Fundamentals/06. MVC_Introduction/MyFirstMvcApp/Controllers/CardsController.cs
@@ -0,0 +1,18 @@
+﻿using SUS.HTTP;
+using SUS.MvcFramework;
+
+namespace MyFirstMvcApp.Controllers
+{
+    public class CardsController : Controller
+    {
+        public HttpResponse All(HttpRequest request)
+        {
+            return this.View();
+        }
+
+        public HttpResponse Collection(HttpRequest request)
+        {
+            return this.View();
+        }
+    }
+}
