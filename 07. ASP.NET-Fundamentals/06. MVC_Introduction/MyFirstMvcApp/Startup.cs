b6ac2f588db9bca74ffbb6d750afc6ac3fde9cd2
mikegsCoder <mikegsid@gmail.com>
2022-10-06 16:33:58 +0300

    Add more routes

--- 07. ASP.NET-Fundamentals/06. MVC_Introduction/MyFirstMvcApp/Startup.cs
+++ 07. ASP.NET-Fundamentals/06. MVC_Introduction/MyFirstMvcApp/Startup.cs
@@ -21,6 +21,16 @@ namespace MyFirstMvcApp
             routeTable.Add(new Route("/users/login", HttpMethod.Get, new UsersController().Login));
             routeTable.Add(new Route("/users/register", HttpMethod.Get, new UsersController().Register));
             routeTable.Add(new Route("/users/login", HttpMethod.Post, new UsersController().DoLogin));
+
+            routeTable.Add(new Route("/cards/all", HttpMethod.Get, new CardsController().All));
+            routeTable.Add(new Route("/cards/add", HttpMethod.Get, new CardsController().Add));
+            routeTable.Add(new Route("/cards/collection", HttpMethod.Get, new CardsController().Collection));
+
+            routeTable.Add(new Route("/favicon.ico", HttpMethod.Get, new StaticFilesController().Favicon));
+            routeTable.Add(new Route("/css/bootstrap.min.css", HttpMethod.Get, new StaticFilesController().BootstrapCss));
+            routeTable.Add(new Route("/css/custom.css", HttpMethod.Get, new StaticFilesController().CustomCss));
+            routeTable.Add(new Route("/js/custom.js", HttpMethod.Get, new StaticFilesController().CustomJs));
+            routeTable.Add(new Route("/js/bootstrap.bundle.min.js", HttpMethod.Get, new StaticFilesController().BootstrapJs));
         }
     }
 }
