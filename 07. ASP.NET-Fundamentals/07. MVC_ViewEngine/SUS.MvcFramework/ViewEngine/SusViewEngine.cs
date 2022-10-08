6a42ecee839ccd6fa013098d16ee6697645c3109
mikegsCoder <mikegsid@gmail.com>
2022-10-08 13:14:48 +0300

    Add GetMethodBody method

--- 07. ASP.NET-Fundamentals/07. MVC_ViewEngine/SUS.MvcFramework/ViewEngine/SusViewEngine.cs
+++ 07. ASP.NET-Fundamentals/07. MVC_ViewEngine/SUS.MvcFramework/ViewEngine/SusViewEngine.cs
@@ -74,5 +74,59 @@ namespace ViewNamespace
 ";
             return csharpCode;
         }
+
+        private string GetMethodBody(string templateCode)
+        {
+            Regex csharpCodeRegex = new Regex(@"[^\""\s&\'\<]+");
+
+            var supportedOperators = new List<string> { "for", "while", "if", "else", "foreach" };
+
+            StringBuilder csharpCode = new StringBuilder();
+
+            StringReader sr = new StringReader(templateCode);
+
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                if (supportedOperators.Any(x => line.TrimStart().StartsWith("@" + x)))
+                {
+                    var atSignLocation = line.IndexOf("@");
+
+                    line = line.Remove(atSignLocation, 1);
+
+                    csharpCode.AppendLine(line);
+                }
+                else if (line.TrimStart().StartsWith("{") ||
+                         line.TrimStart().StartsWith("}"))
+                {
+                    csharpCode.AppendLine(line);
+                }
+                else
+                {
+                    csharpCode.Append($"html.AppendLine(@\"");
+
+                    while (line.Contains("@"))
+                    {
+                        var atSignLocation = line.IndexOf("@");
+
+                        var htmlBeforeAtSign = line.Substring(0, atSignLocation);
+
+                        csharpCode.Append(htmlBeforeAtSign.Replace("\"", "\"\"") + "\" + ");
+
+                        var lineAfterAtSign = line.Substring(atSignLocation + 1);
+
+                        var code = csharpCodeRegex.Match(lineAfterAtSign).Value;
+
+                        csharpCode.Append(code + " + @\"");
+
+                        line = lineAfterAtSign.Substring(code.Length);
+                    }
+
+                    csharpCode.AppendLine(line.Replace("\"", "\"\"") + "\");");
+                }
+            }
+
+            return csharpCode.ToString();
+        }
     }
 }
