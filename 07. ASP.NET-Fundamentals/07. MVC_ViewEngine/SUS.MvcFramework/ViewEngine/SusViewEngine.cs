f053dbff7c9cf3e268adf5515a6cf5a1a39321a4
mikegsCoder <mikegsid@gmail.com>
2022-10-08 13:11:03 +0300

    Add SusViewEngine class

---
+++ 07. ASP.NET-Fundamentals/07. MVC_ViewEngine/SUS.MvcFramework/ViewEngine/SusViewEngine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using Microsoft.CodeAnalysis;
+using System.Collections.Generic;
+using Microsoft.CodeAnalysis.Emit;
+using Microsoft.CodeAnalysis.CSharp;
+using System.Text.RegularExpressions;
+
+namespace SUS.MvcFramework.ViewEngine
+{
+    public class SusViewEngine : IViewEngine
+    {
+        public string GetHtml(string templateCode, object viewModel, string user)
+        {
+            string csharpCode = GenerateCSharpFromTemplate(templateCode, viewModel);
+
+            IView executableObject = GenerateExecutableCоde(csharpCode, viewModel);
+
+            string html = executableObject.ExecuteTemplate(viewModel, user);
+
+            return html;
+        }
+    }
+}
