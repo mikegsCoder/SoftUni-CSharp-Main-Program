d74cdf19f6610b90b22ca1888e07cffe5ebed9f8
mikegsCoder <mikegsid@gmail.com>
2022-10-08 13:16:36 +0300

    Add GenerateExecutableCоde method

--- 07. ASP.NET-Fundamentals/07. MVC_ViewEngine/SUS.MvcFramework/ViewEngine/SusViewEngine.cs
+++ 07. ASP.NET-Fundamentals/07. MVC_ViewEngine/SUS.MvcFramework/ViewEngine/SusViewEngine.cs
@@ -128,5 +128,71 @@ namespace ViewNamespace
 
             return csharpCode.ToString();
         }
+
+        private IView GenerateExecutableCоde(string csharpCode, object viewModel)
+        {
+            var compileResult = CSharpCompilation.Create("ViewAssembly")
+                .WithOptions(new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary))
+                .AddReferences(MetadataReference.CreateFromFile(typeof(object).Assembly.Location))
+                .AddReferences(MetadataReference.CreateFromFile(typeof(IView).Assembly.Location));
+
+            if (viewModel != null)
+            {
+                if (viewModel.GetType().IsGenericType)
+                {
+                    var genericArguments = viewModel.GetType().GenericTypeArguments;
+
+                    foreach (var genericArgument in genericArguments)
+                    {
+                        compileResult = compileResult
+                            .AddReferences(MetadataReference.CreateFromFile(genericArgument.Assembly.Location));
+                    }
+                }
+
+                compileResult = compileResult
+                    .AddReferences(MetadataReference.CreateFromFile(viewModel.GetType().Assembly.Location));
+            }
+
+            var libraries = Assembly.Load(new AssemblyName("netstandard"))
+                .GetReferencedAssemblies();
+
+            foreach (var library in libraries)
+            {
+                compileResult = compileResult
+                    .AddReferences(MetadataReference.CreateFromFile(
+                        Assembly.Load(library).Location));
+            }
+
+            compileResult = compileResult.AddSyntaxTrees(SyntaxFactory.ParseSyntaxTree(csharpCode));
+
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                EmitResult result = compileResult.Emit(memoryStream);
+
+                if (!result.Success)
+                {
+                    return new ErrorView(result.Diagnostics.Where(x => x.Severity == DiagnosticSeverity.Error).Select(x => x.GetMessage()), csharpCode);
+                }
+
+                try
+                {
+                    memoryStream.Seek(0, SeekOrigin.Begin);
+
+                    var byteAssembly = memoryStream.ToArray();
+
+                    var assembly = Assembly.Load(byteAssembly);
+
+                    var viewType = assembly.GetType("ViewNamespace.ViewClass");
+
+                    var instance = Activator.CreateInstance(viewType);
+
+                    return (instance as IView) ?? new ErrorView(new List<string> { "Instance is null!" }, csharpCode);
+                }
+                catch (Exception ex)
+                {
+                    return new ErrorView(new List<string> { ex.ToString() }, csharpCode);
+                }
+            }
+        }
     }
 }
