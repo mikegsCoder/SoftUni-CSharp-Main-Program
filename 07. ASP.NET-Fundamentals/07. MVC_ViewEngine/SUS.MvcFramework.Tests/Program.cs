0ee0fa87fc54ca0e6ef3d9878114caaad2f600dd
mikegsCoder <mikegsid@gmail.com>
2022-10-09 11:52:31 +0300

    Add new project SUS.MvcFramework.Tests

---
+++ 07. ASP.NET-Fundamentals/07. MVC_ViewEngine/SUS.MvcFramework.Tests/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SUS.MvcFramework.Tests
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
