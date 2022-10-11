62d63d318d707aba56c7e15a34049aecc40d920b
mikegsCoder <mikegsid@gmail.com>
2022-10-11 14:44:37 +0300

    Add MVC-Intro-Demo

---
+++ 07. ASP.NET-Fundamentals/08. MVC-Intro-Demo/MVC-Intro-Demo/MVC-Intro-Demo/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Intro_Demo.Models;
+using System.Diagnostics;
+using System.Text.Json;
+
+namespace MVC_Intro_Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Hello World!";
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
