4b2282032ee7014508da2c972b3858dcbfbcec0e
mikegsCoder <mikegsid@gmail.com>
2022-10-12 15:05:21 +0300

    Add ProductsController

---
+++ 07. ASP.NET-Fundamentals/08. MVC-Intro-Demo/MVC-Intro-Demo/MVC-Intro-Demo/Controllers/ProductsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Intro_Demo.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
