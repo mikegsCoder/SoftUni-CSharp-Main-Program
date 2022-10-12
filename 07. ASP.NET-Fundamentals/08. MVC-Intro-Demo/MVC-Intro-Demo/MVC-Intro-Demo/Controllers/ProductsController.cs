9b18a81719f2556787ec45cc5dee5dfbc864914c
mikegsCoder <mikegsid@gmail.com>
2022-10-12 15:08:30 +0300

    Add ProductViewModel

--- 07. ASP.NET-Fundamentals/08. MVC-Intro-Demo/MVC-Intro-Demo/MVC-Intro-Demo/Controllers/ProductsController.cs
+++ 07. ASP.NET-Fundamentals/08. MVC-Intro-Demo/MVC-Intro-Demo/MVC-Intro-Demo/Controllers/ProductsController.cs
@@ -1,9 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Intro_Demo.ViewModels;
 
 namespace MVC_Intro_Demo.Controllers
 {
     public class ProductsController : Controller
     {
+        private IEnumerable<ProductViewModel> products = new List<ProductViewModel>
+        {
+            new ProductViewModel()
+            {
+                Id = 1,
+                Name = "Cheese",
+                Price = 7.00
+            },
+
+            new ProductViewModel()
+            {
+                Id = 2,
+                Name = "Ham",
+                Price = 5.50
+            },
+
+            new ProductViewModel()
+            {
+                Id = 3,
+                Name = "Bread",
+                Price = 1.50
+            }
+        };
+
         public IActionResult Index()
         {
             return View();
