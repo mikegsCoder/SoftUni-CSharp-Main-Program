60b1508e2d748409a36bca6c1e5b9d677555cbdb
mikegsCoder <mikegsid@gmail.com>
2022-10-14 16:26:12 +0300

    Add ChatController

---
+++ 07. ASP.NET-Fundamentals/08. MVC-Intro-Demo/ChatApp/ChatApp/Controllers/ChatController.cs
@@ -0,0 +1,31 @@
+﻿using ChatApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApp.Controllers
+{
+    public class ChatController : Controller
+    {
+        private static List<KeyValuePair<string, string>> Messages = new List<KeyValuePair<string, string>>();
+
+        public IActionResult Show()
+        {
+            if(Messages.Count < 0)
+            {
+                return View(new ChatViewModel());
+            }
+            var chatModel = new ChatViewModel()
+            {
+                Messages = Messages
+                .Select(m => new MessageViewModel()
+                {
+                    Sender = m.Key,
+                    MessageText = m.Value
+                })
+                .ToList()
+            };
+
+            return View(chatModel);
+        }
+
+    }
+}
