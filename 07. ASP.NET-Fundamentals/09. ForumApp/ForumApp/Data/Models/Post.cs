420865dd9597ad3b15ffd60fcdef313e40358ed2
mikegsCoder <mikegsid@gmail.com>
2022-10-16 12:44:36 +0300

    Add Post data model

---
+++ 07. ASP.NET-Fundamentals/09. ForumApp/ForumApp/Data/Models/Post.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using static ForumApp.Constants.DataConstants.Post;
+
+namespace ForumApp.Data.Models
+{
+    [Comment("Published posts")]
+    public class Post
+    {
+        [Key]
+        [Comment("Posts Identifier")]
+        public int Id { get; set; }
+
+        [Required]
+        [Comment("Post Title")]
+        [MaxLength(TitleMaxLength)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [Comment("Content")]
+        [MaxLength(ContentMaxLength)]
+        public string Content { get; set; } = null!;
+
+        [Required]
+        [Comment("Mark record as deleted")]
+       
+        public bool IsDeleted { get; set; } = false;
+    }
+}
