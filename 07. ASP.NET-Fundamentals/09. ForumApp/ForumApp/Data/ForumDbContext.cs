2303251f4a669bdff8d5fa10b23627390d5f66b5
mikegsCoder <mikegsid@gmail.com>
2022-10-16 12:42:29 +0300

    Add ForumApp initial commit

---
+++ 07. ASP.NET-Fundamentals/09. ForumApp/ForumApp/Data/ForumDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ForumApp.Data
+{
+    public class ForumDbContext : DbContext
+    {
+        public ForumDbContext(DbContextOptions<ForumDbContext> options)
+            :base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
