9e9165350a8b090e962cbc7f1cb3664ce6bf5dd4
mikegsCoder <mikegsid@gmail.com>
2022-10-16 12:53:00 +0300

    Add constants

---
+++ 07. ASP.NET-Fundamentals/09. ForumApp/ForumApp/Constants/DataConstants.cs
@@ -0,0 +1,24 @@
+﻿namespace ForumApp.Constants
+{
+    /// <summary>
+    /// Data layer constants
+    /// </summary>
+    public static class DataConstants
+    {
+        /// <summary>
+        /// Posts constants
+        /// </summary>
+        public static class Post
+        {
+            /// <summary>
+            /// Max title length
+            /// </summary>
+            public const int TitleMaxLength = 50;
+
+            /// <summary>
+            /// Max content length
+            /// </summary>
+            public const int ContentMaxLength = 1500;
+        }
+    }
+}
