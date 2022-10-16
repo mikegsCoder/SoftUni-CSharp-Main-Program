57859d4a66041413803c13567d480cec41b6c54a
mikegsCoder <mikegsid@gmail.com>
2022-10-16 12:55:26 +0300

    Add migration CreateAppDB

---
+++ 07. ASP.NET-Fundamentals/09. ForumApp/ForumApp/Migrations/20221016095359_CreateAppDB.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ForumApp.Migrations
+{
+    public partial class CreateAppDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false, comment: "Posts Identifier")
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false, comment: "Post Title"),
+                    Content = table.Column<string>(type: "nvarchar(1500)", maxLength: 1500, nullable: false, comment: "Content"),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false, defaultValue: false, comment: "Mark record as deleted")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                },
+                comment: "Published posts");
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { 1, "My post will be about performing CRUD operations in MVC. It`s so much fun!", "My first post" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { 2, "This is my second post. CRUD operations in MVC are getting more and more interesting!", "My second post" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { 3, "Hello, there. I am getting better and better with the CRUD operations in MVC. Stayed tuned!", "My third post" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}
