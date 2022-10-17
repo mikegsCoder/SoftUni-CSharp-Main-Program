9612e958f56b3e66d621cabcb2fdc06922f10579
mikegsCoder <mikegsid@gmail.com>
2022-10-17 16:35:08 +0300

    Add PostViewModel

---
+++ 07. ASP.NET-Fundamentals/09. ForumApp/ForumApp/Models/PostViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ForumApp.Models
+{
+    public class PostViewModel : AddPostViewModel
+    {
+        
+        public int Id { get; set; }
+        
+    }
+}
