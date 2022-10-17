c66c8cd0065b0c0e5112abf1a7b98936034d5ef5
mikegsCoder <mikegsid@gmail.com>
2022-10-17 16:36:44 +0300

    Add AddPostViewModel

---
+++ 07. ASP.NET-Fundamentals/09. ForumApp/ForumApp/Models/AddPostViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ForumApp.Models
+{
+    public class AddPostViewModel
+    {
+        [Display(Name = "Title")]
+        [Required(ErrorMessage = "The field is required!")]
+        [StringLength(50, MinimumLength = 10, ErrorMessage = "The field {0} must be between {2} and {1} symbols.")]
+        public string Title { get; set; }
+
+        [Display(Name = "Content")]
+        [Required(ErrorMessage = "The field is required!")]
+        [StringLength(1500, MinimumLength = 30, ErrorMessage = "The field {0} must be between {2} and {1} symbols.")]
+        public string Content { get; set; }
+    }
+}
