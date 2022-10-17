362caad206292a07fe673b2f356b11d5a08c8c99
mikegsCoder <mikegsid@gmail.com>
2022-10-17 16:38:22 +0300

    Add PostController

---
+++ 07. ASP.NET-Fundamentals/09. ForumApp/ForumApp/Controllers/PostController.cs
@@ -0,0 +1,33 @@
+﻿using ForumApp.Data;
+using ForumApp.Data.Models;
+using ForumApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ForumApp.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly ForumDbContext context;
+
+        public PostController(ForumDbContext _context)
+        {
+            context = _context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var model = await context
+                .Posts
+                .Where(p => p.IsDeleted == false)
+                .Select(p => new PostViewModel()
+                {
+                    Id = p.Id,
+                    Title = p.Title,
+                    Content = p.Content
+                })
+                .ToListAsync();
+
+            return View(model);
+        }
+    }
+}
