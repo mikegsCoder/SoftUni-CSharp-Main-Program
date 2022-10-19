c5bf5cddd06209514edc89ae4625bc0e08304442
mikegsCoder <mikegsid@gmail.com>
2022-10-19 12:12:50 +0300

    Add Board

---
+++ 07. ASP.NET-Fundamentals/10. TaskBoardApp/TaskBoardApp/Data/Entities/Board.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static TaskBoardApp.Data.DataConstants.Board;
+
+namespace TaskBoardApp.Data.Entities
+{
+    public class Board
+    {
+        public int Id { get; init; }
+        [Required]
+        [MaxLength(MaxBoardName)]
+        public string Name { get; set; }
+
+        public IEnumerable<Task> Tasks { get; set; } = new List<Task>();
+    }
+}
