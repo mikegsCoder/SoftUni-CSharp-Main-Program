e2286d47b57b7c3d1d0c81b8e07591d6d2556fbc
mikegsCoder <mikegsid@gmail.com>
2022-10-19 12:11:51 +0300

    Add User

---
+++ 07. ASP.NET-Fundamentals/10. TaskBoardApp/TaskBoardApp/Data/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using static TaskBoardApp.Data.DataConstants.User;
+
+namespace TaskBoardApp.Data.Entities
+{
+    public class User : IdentityUser
+    {
+        [Required]
+        [MaxLength(MaxUserFirstName)]
+        public string FirstName { get; init; }
+
+        [Required]
+        [MaxLength(MaxUserLastName)]
+        public string LastName { get; init; }
+    }
+}
