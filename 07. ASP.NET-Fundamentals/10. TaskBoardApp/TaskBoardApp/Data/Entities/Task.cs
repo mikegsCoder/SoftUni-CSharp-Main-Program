7c7567d0965045d0b833069ddd4d17408c358918
mikegsCoder <mikegsid@gmail.com>
2022-10-19 12:13:33 +0300

    Add Task

---
+++ 07. ASP.NET-Fundamentals/10. TaskBoardApp/TaskBoardApp/Data/Entities/Task.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static TaskBoardApp.Data.DataConstants.Task;
+
+namespace TaskBoardApp.Data.Entities
+{
+    public class Task
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(MaxTaskTitle)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(MaxTaskDescription)]
+        public string Description { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public int BoardId { get; set; }
+
+        public Board Board { get; init; }
+        [Required]
+        public string OwnerId { get; set; }
+
+        public User Owner { get; init; }
+
+    }
+}
