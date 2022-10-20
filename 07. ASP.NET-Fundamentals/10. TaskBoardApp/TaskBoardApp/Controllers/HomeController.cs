c487ea0d827553eadc58435cbfa2433e975b9065
mikegsCoder <mikegsid@gmail.com>
2022-10-20 12:50:21 +0300

    Add functionality in HomeController

--- 07. ASP.NET-Fundamentals/10. TaskBoardApp/TaskBoardApp/Controllers/HomeController.cs
+++ 07. ASP.NET-Fundamentals/10. TaskBoardApp/TaskBoardApp/Controllers/HomeController.cs
@@ -1,14 +1,53 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using System.Security.Claims;
+using TaskBoardApp.Data;
 using TaskBoardApp.Models;
 
 namespace TaskBoardApp.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly TaskBoardAppDbContext data;
+
+        public HomeController(TaskBoardAppDbContext data)
         {
-            return View();
+            this.data = data;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var taskBoards = await this.data.Boards.Select(b => b.Name).Distinct().ToListAsync();
+
+            var tasksCounts = new List<HomeBoardModel>();
+
+            foreach (var boardName in taskBoards)
+            {
+                var tasksInBoard = await this.data.Tasks.Where(t => t.Board.Name == boardName).CountAsync();
+                tasksCounts.Add(new HomeBoardModel()
+                {
+                    BoardName = boardName,
+                    TasksCount = tasksInBoard
+                });
+            }
+
+            var userTasksCount = -1;
+            if (this.User?.Identity?.IsAuthenticated ?? false)
+            {
+                var currentUserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+                userTasksCount = await this.data.Tasks.Where(t => t.OwnerId == currentUserId).CountAsync();
+
+            }
+
+            var homeModel = new HomeViewModel()
+            {
+                AllTasksCount = this.data.Tasks.Count(),
+                BoardsWithTasksCount = tasksCounts,
+                UserTasksCount = userTasksCount
+            };
+
+            return View(homeModel);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
