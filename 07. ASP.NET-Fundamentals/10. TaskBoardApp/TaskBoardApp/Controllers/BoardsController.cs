57121074c1d9e66306fb12c467f6370756c09e58
mikegsCoder <mikegsid@gmail.com>
2022-10-21 12:37:37 +0300

    Add BoardsController

---
+++ 07. ASP.NET-Fundamentals/10. TaskBoardApp/TaskBoardApp/Controllers/BoardsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskBoardApp.Data;
+using TaskBoardApp.Models;
+using TaskBoardApp.Models.Tasks;
+using Task = TaskBoardApp.Data.Entities.Task;
+
+namespace TaskBoardApp.Controllers
+{
+    public class BoardsController : Controller
+    {
+        private readonly TaskBoardAppDbContext data;
+
+        public BoardsController(TaskBoardAppDbContext context)
+        {
+            this.data = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            var boards = data.Boards
+                .Select(b => new BoardViewModel()
+                {
+                    Id = b.Id,
+                    Name = b.Name,
+                    Tasks = b.Tasks.Select(t => new TaskViewModel()
+                    {
+                        Id = t.Id,
+                        Title = t.Title,
+                        Description = t.Description,
+                        Owner = t.Owner.UserName
+                    })
+                })
+                .ToListAsync();
+
+            return View(boards);
+        }
+    }
+}
