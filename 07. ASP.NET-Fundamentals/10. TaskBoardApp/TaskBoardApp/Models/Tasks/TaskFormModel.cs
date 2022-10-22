0c724559a84e851f255ad4df8645eced3091076a
mikegsCoder <mikegsid@gmail.com>
2022-10-22 12:51:58 +0300

    Add TaskFormModel

---
+++ 07. ASP.NET-Fundamentals/10. TaskBoardApp/TaskBoardApp/Models/Tasks/TaskFormModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static TaskBoardApp.Data.DataConstants.Task;
+
+namespace TaskBoardApp.Models.Tasks
+{
+    public class TaskFormModel
+    {
+        [Required]
+        [StringLength(MaxTaskTitle, MinimumLength = MinTaskTitle, ErrorMessage = "Title should be at least {2} characters long.")]
+
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(MaxTaskDescription, MinimumLength = MinTaskDescription, ErrorMessage = "Description should be at least {2} characters long.")]
+        public string Description { get; set; }
+
+        [Display(Name ="Board")]
+        public int BoardId { get; set; }
+
+        public IEnumerable<TaskBoardModel> Boards { get; set; } = new List<TaskBoardModel>();
+    }
+}
