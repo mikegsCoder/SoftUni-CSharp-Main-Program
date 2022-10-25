01333b6d1acae2550e11ec1d48ccbbf891788280
mikegsCoder <mikegsid@gmail.com>
2022-10-25 17:18:04 +0300

    Add User model

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/01. Watchlist/02. Solution/Watchlist/Watchlist/Data/Models/User.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Watchlist.Data.Models
+{
+    public class User : IdentityUser
+    {
+        public ICollection<UserMovie> UsersMovies { get; set; } = new List<UserMovie>();
+    }
+}
