e2053769fc92555e22d8ae5414dd2ce6d04ad3e4
mikegsCoder <mikegsid@gmail.com>
2022-10-26 17:12:10 +0300

    Add functionality in WatchlistDbContext

--- 07. ASP.NET-Fundamentals/11. Exam_Preparation/01. Watchlist/02. Solution/Watchlist/Data/WatchlistDbContext.cs
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/01. Watchlist/02. Solution/Watchlist/Data/WatchlistDbContext.cs
@@ -1,18 +1,51 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Watchlist.Data.Models;
 
 namespace Watchlist.Data
 {
-    public class WatchlistDbContext : IdentityDbContext
+    public class WatchlistDbContext : IdentityDbContext<User>
     {
         public WatchlistDbContext(DbContextOptions<WatchlistDbContext> options)
             : base(options)
         {
         }
 
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<UserMovie> UsersMovies { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
-           /* builder
+            // props resrictions:
+            builder.Entity<User>()
+                .Property(x => x.UserName)
+                .HasMaxLength(20)
+                .IsRequired();
+
+            builder.Entity<User>()
+                .Property(x => x.Email)
+                .HasMaxLength(60)
+                .IsRequired();
+
+            // composite key:
+            builder.Entity<UserMovie>()
+                .HasKey(x => new { x.UserId, x.MovieId });
+
+            // decimal props:
+            var decimalProps = builder.Model
+               .GetEntityTypes()
+               .SelectMany(t => t.GetProperties())
+               .Where(p => (System.Nullable.GetUnderlyingType(p.ClrType) ?? p.ClrType) == typeof(decimal));
+
+            foreach (var property in decimalProps)
+            {
+                property.SetPrecision(18);
+                property.SetScale(2);
+            }
+
+            // seeding:
+            builder
                 .Entity<Genre>()
                 .HasData(new Genre()
                 {
@@ -39,7 +72,7 @@ namespace Watchlist.Data
                     Id = 5,
                     Name = "Romantic"
                 });
-           */
+
             base.OnModelCreating(builder);
         }
     }
