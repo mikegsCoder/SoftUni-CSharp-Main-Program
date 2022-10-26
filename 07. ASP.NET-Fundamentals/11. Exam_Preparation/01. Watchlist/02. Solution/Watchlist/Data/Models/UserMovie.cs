7d24ac92730ca4beb4d5be66d9988e6a3d09e09d
mikegsCoder <mikegsid@gmail.com>
2022-10-26 17:07:10 +0300

    Add UserMovie data model

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/01. Watchlist/02. Solution/Watchlist/Data/Models/UserMovie.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Watchlist.Data.Models
+{
+    public class UserMovie
+    {
+        [Required]
+        [ForeignKey(nameof(User))]
+        public string UserId { get; set; }
+
+        public User User { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Movie))]
+        public int MovieId { get; set; }
+
+        public Movie Movie { get; set; }
+    }
+}
