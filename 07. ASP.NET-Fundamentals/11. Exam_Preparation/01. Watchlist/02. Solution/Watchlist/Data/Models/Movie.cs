d22c3a717b99cd49e263409836a893663642ea3e
mikegsCoder <mikegsid@gmail.com>
2022-10-26 17:05:40 +0300

    Add Movie data model

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/01. Watchlist/02. Solution/Watchlist/Data/Models/Movie.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Build.Execution;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Watchlist.Data.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Director { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        [Range(0.00, 10.00)]
+        public decimal Rating { get; set; }
+
+        [ForeignKey(nameof(Genre))]
+        public int GenreId { get; set; }
+
+        public Genre Genre { get; set; }
+
+        public ICollection<UserMovie> UsersMovies { get; set; } = new List<UserMovie>();
+    }
+}
