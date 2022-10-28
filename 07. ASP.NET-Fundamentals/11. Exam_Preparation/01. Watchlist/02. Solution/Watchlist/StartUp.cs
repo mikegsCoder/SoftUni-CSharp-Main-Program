4e71b730d28a26014c59586c948f0461b1a09dd2
mikegsCoder <mikegsid@gmail.com>
2022-10-28 15:57:18 +0300

    Add configurations in StartUp

--- 07. ASP.NET-Fundamentals/11. Exam_Preparation/01. Watchlist/02. Solution/Watchlist/StartUp.cs
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/01. Watchlist/02. Solution/Watchlist/StartUp.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Watchlist.Data;
+using Watchlist.Data.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,8 +10,22 @@ builder.Services.AddDbContext<WatchlistDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<User>(options =>
+{
+    options.Password.RequiredLength = 5;
+    options.SignIn.RequireConfirmedAccount = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+})
     .AddEntityFrameworkStores<WatchlistDbContext>();
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/User/Login";
+});
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
