13e285be7a57e15287c94c001caac8208f487ef6
mikegsCoder <mikegsid@gmail.com>
2022-10-28 16:02:13 +0300

    Add UserController with HttpGet Register action

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/01. Watchlist/02. Solution/Watchlist/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Watchlist.Data.Models;
+using Watchlist.Models;
+
+namespace Watchlist.Controllers
+{
+    [Authorize]
+    public class UserController : Controller
+    {
+        private readonly UserManager<User> userManager;
+        private readonly SignInManager<User> signInManager;
+
+        public UserController(
+            UserManager<User> _userManager,
+            SignInManager<User> _signInManager)
+        {
+            userManager = _userManager;
+            signInManager = _signInManager;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            if (User?.Identity?.IsAuthenticated ?? false)
+            {
+                return RedirectToAction("All", "Movies"); // Where to redirect logged in user
+            }
+
+            var model = new RegisterViewModel();
+
+            return View(model);
+        }
+
+    }
+}
