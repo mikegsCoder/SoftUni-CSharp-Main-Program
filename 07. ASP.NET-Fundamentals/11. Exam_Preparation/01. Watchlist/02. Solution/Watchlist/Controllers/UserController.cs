f03242336814a8a5c3d28fb4213377d2fb42be70
mikegsCoder <mikegsid@gmail.com>
2022-10-29 11:26:58 +0300

    Add HttpPots Login action

--- 07. ASP.NET-Fundamentals/11. Exam_Preparation/01. Watchlist/02. Solution/Watchlist/Controllers/UserController.cs
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/01. Watchlist/02. Solution/Watchlist/Controllers/UserController.cs
@@ -77,5 +77,31 @@ namespace Watchlist.Controllers
 
             return View(model);
         }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = await userManager.FindByNameAsync(model.UserName);
+
+            if (user != null)
+            {
+                var result = await signInManager.PasswordSignInAsync(user, model.Password, false, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("All", "Movies"); // Where to redirect logged in user
+                }
+            }
+
+            ModelState.AddModelError("", "Invalid login");
+
+            return View(model);
+        }
     }
 }
