2eeac16e1c4e6f0c00a63b69a58fffb5407be890
mikegsCoder <mikegsid@gmail.com>
2022-10-29 11:35:30 +0300

    Add MovieController

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/01. Watchlist/02. Solution/Watchlist/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Watchlist.Models;
+
+namespace Watchlist.Controllers
+{
+    [Authorize]
+    public class MoviesController : Controller
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+
+            return View();
+        }
+    }
+}
