0b095827f044d93a7e98c0e5517527a8bf0ca355
mikegsCoder <mikegsid@gmail.com>
2022-10-29 11:36:48 +0300

    Add MovieViewModel

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/01. Watchlist/02. Solution/Watchlist/Models/MovieViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Watchlist.Models
+{
+    public class MovieViewModel
+    {
+        public string Title { get; set; }
+
+        public string Director{ get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Rating { get; set; }
+
+        public string Genre { get; set; }
+
+        public int Id { get; set; }
+    }
+}
