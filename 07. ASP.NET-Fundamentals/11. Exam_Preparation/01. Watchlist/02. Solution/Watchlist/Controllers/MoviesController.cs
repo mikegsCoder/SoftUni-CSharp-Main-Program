8eaf1b36722ea8cc6c6449d3fabca118662e6c86
mikegsCoder <mikegsid@gmail.com>
2022-10-30 10:37:09 +0200

    Implemented All HttpGet action

--- 07. ASP.NET-Fundamentals/11. Exam_Preparation/01. Watchlist/02. Solution/Watchlist/Controllers/MoviesController.cs
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/01. Watchlist/02. Solution/Watchlist/Controllers/MoviesController.cs
@@ -1,19 +1,28 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
+using Watchlist.Contracts;
 using Watchlist.Models;
+using Watchlist.Services;
 
 namespace Watchlist.Controllers
 {
     [Authorize]
     public class MoviesController : Controller
     {
+        private readonly IMovieService movieService;
+
+        public MoviesController(IMovieService _movieService)
+        {
+            movieService = _movieService;
+        }
 
         [HttpGet]
         public async Task<IActionResult> All()
         {
+            var model = await movieService.GetAllAsync();
 
-            return View();
+            return View(model);
         }
     }
 }
