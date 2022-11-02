f14349bd144d56193289e94a2f7145bf18925fd7
mikegsCoder <mikegsid@gmail.com>
2022-11-02 15:31:04 +0200

    Add Book data model

---
+++ 07. ASP.NET-Fundamentals/12. My_Exam/02. Solution/Library/Data/Models/Book.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Library.Data.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Author { get; set; }
+
+        [Required]
+        [MaxLength(5000)]
+        public string Description { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        [Range(0.00, 10.00)]
+        public decimal Rating { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Category))]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public Category Category { get; set; }
+
+        public ICollection<ApplicationUserBook> ApplicationUsersBooks { get; set; } = new List<ApplicationUserBook>();
+    }
+}
