b20a7cda2a08736093ceebde8bb936a8c13d292d
mikegsCoder <mikegsid@gmail.com>
2022-11-04 06:59:17 +0200

    Add LoginViewModel

---
+++ 07. ASP.NET-Fundamentals/12. My_Exam/02. Solution/Library/Models/User/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.Models.User
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "The field is required!")]
+        public string UserName { get; set; } = null!;
+
+        [Required(ErrorMessage = "The field is required!")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+    }
+}
