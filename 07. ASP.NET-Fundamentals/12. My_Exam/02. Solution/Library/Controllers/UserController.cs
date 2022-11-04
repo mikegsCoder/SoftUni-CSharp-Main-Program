85bbb36913e540a46498c9289fc4dc38acb633b6
mikegsCoder <mikegsid@gmail.com>
2022-11-04 07:02:46 +0200

    Add UserController initial commit

---
+++ 07. ASP.NET-Fundamentals/12. My_Exam/02. Solution/Library/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Library.Data.Models;
+using Library.Models.User;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Controllers
+{
+    [Authorize]
+    public class UserController : Controller
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public UserController(
+            UserManager<ApplicationUser> _userManager,
+            SignInManager<ApplicationUser> _signInManager)
+        {
+            userManager = _userManager;
+            signInManager = _signInManager;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            if (User?.Identity?.IsAuthenticated ?? false)
+            {
+                return RedirectToAction("All", "Books"); // redirect logged in user
+            }
+
+            var model = new RegisterViewModel();
+
+            return View(model);
+        }
+    }
+}
