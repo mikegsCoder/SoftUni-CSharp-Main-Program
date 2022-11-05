3ce1ed1e215068585421a45232a0802f88268132
mikegsCoder <mikegsid@gmail.com>
2022-11-05 16:51:05 +0200

    Add BookViewModel

---
+++ 07. ASP.NET-Fundamentals/12. My_Exam/02. Solution/Library/Models/Book/BookViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Library.Models.Book
+{
+    public class BookViewModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Description { get; set; }
+
+        public string Rating { get; set; }
+
+        public string Category { get; set; }
+    }
+}
