1248af7747a1c4afc08c5bf406e883cc7647f321
mikegsCoder <mikegsid@gmail.com>
2022-11-06 11:42:53 +0200

    Add BooksController initial commit

---
+++ 07. ASP.NET-Fundamentals/12. My_Exam/02. Solution/Library/Controllers/BooksController.cs
@@ -0,0 +1,19 @@
+﻿using Library.Contracts;
+using Library.Models.Book;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Library.Controllers
+{
+    [Authorize]
+    public class BooksController : Controller
+    {
+       
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            return View();
+        }
+    }
+}
