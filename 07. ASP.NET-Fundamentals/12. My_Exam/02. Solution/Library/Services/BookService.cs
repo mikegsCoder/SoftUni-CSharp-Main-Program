d0ce692db07eb62662b507544c2b24b9b6628a92
mikegsCoder <mikegsid@gmail.com>
2022-11-06 11:46:16 +0200

    Add BookService

---
+++ 07. ASP.NET-Fundamentals/12. My_Exam/02. Solution/Library/Services/BookService.cs
@@ -0,0 +1,36 @@
+﻿using Library.Contracts;
+using Library.Data.Models;
+using Library.Data;
+using Library.Models.Book;
+using System.Globalization;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Services
+{
+    public class BookService : IBookService
+    {
+        private readonly LibraryDbContext context;
+
+        public BookService(LibraryDbContext _context)
+        {
+            context = _context;
+        }
+
+        public async Task<IEnumerable<BookViewModel>> GetAllAsync()
+        {
+            return await context.Books
+                .Include(b => b.Category)
+                .Select(b => new BookViewModel()
+                {
+                    Id = b.Id,
+                    Title = b.Title,
+                    Author = b.Author,
+                    ImageUrl = b.ImageUrl,
+                    Description = b.Description,
+                    Rating = b.Rating.ToString("F2"),
+                    Category = b.Category.Name
+                })
+                .ToListAsync();
+        }
+    }
+}
