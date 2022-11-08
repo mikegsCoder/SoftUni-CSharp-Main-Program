cc8dc76b9268574e4feb8c5cdc4787f5638ad5ad
mikegsCoder <mikegsid@gmail.com>
2022-11-08 15:14:20 +0200

    Add CategoryService

---
+++ 07. ASP.NET-Fundamentals/12. My_Exam/02. Solution/Library/Services/CategoryService.cs
@@ -0,0 +1,20 @@
+﻿using Library.Contracts;
+using Library.Data.Models;
+using Library.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly LibraryDbContext context;
+
+        public CategoryService(LibraryDbContext _context)
+        {
+            context = _context;
+        }
+
+        public async Task<IEnumerable<Category>> GetCategoriesAsync()
+            => await context.Categories.ToListAsync();
+    }
+}
