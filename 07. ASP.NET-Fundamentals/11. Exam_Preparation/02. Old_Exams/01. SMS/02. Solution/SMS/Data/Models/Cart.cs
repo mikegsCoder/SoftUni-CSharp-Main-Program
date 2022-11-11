51ebf0c3f12890ce9683ec42460fcc0ac3ab86fd
mikegsCoder <mikegsid@gmail.com>
2022-11-11 16:38:30 +0200

    Add Cart data model

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/01. SMS/02. Solution/SMS/Data/Models/Cart.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.Data.Models
+{
+    public class Cart
+    {
+        [Key]
+        [StringLength(36)]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        [Required]
+        public User User { get; set; }
+
+        [Required]
+        public ICollection<Product> Products { get; set; } = new List<Product>();
+    }
+}
