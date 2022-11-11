cf05b417bc0a3136157c20cbd2d7ce28d13dc1e4
mikegsCoder <mikegsid@gmail.com>
2022-11-11 16:41:00 +0200

    Fixed configuration string

--- 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/01. SMS/02. Solution/SMS/Data/DatabaseConfiguration.cs
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/01. SMS/02. Solution/SMS/Data/DatabaseConfiguration.cs
@@ -4,6 +4,6 @@
     {
         // ReSharper disable once InconsistentNaming
         public const string ConnectionString =
-            @"Server=.\SQLEXPRESS;Database=SMS;Trusted_Connection=True;Integrated Security=True;";
+            @"Server=.;Database=SMS;Trusted_Connection=True;Integrated Security=True;";
     }
 }
