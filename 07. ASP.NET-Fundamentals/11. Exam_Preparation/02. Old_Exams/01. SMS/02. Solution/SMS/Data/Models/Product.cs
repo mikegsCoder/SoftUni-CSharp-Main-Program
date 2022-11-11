06b0e8851a7d26f6f30bec39c7048ceaa370c08a
mikegsCoder <mikegsid@gmail.com>
2022-11-11 16:39:32 +0200

    Add Product data model

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/01. SMS/02. Solution/SMS/Data/Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.Data.Models
+{
+    public class Product
+    {
+        [Key]
+        [StringLength(36)]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        [Required]
+        [StringLength(20)]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(0.05, 1000)]
+        [Column(TypeName = "money")]
+        public decimal Price { get; set; }
+
+        [StringLength(36)]
+        public string CartId { get; set; }
+
+        [ForeignKey(nameof(CartId))]
+        public Cart Cart { get; set; }
+    }
+}
