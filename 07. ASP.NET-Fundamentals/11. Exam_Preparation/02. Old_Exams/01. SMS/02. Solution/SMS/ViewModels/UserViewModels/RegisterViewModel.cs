c05c3fcfdbc94e354db521fd953c723dc149ec0b
mikegsCoder <mikegsid@gmail.com>
2022-11-12 12:12:16 +0200

    Add RegisterViewModel

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/01. SMS/02. Solution/SMS/ViewModels/UserViewModels/RegisterViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.ViewModels.UserViewModels
+{
+    public class RegisterViewModel
+    {
+        [StringLength(20, MinimumLength = 5, ErrorMessage = "{0} must be between {2} and {1} characters")]
+        public string Username { get; set; }
+
+        [EmailAddress(ErrorMessage = "Email must be valid email")]
+        public string Email { get; set; }
+
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "{0} must be between {2} and {1} characters")]
+        public string Password { get; set; }
+
+        [Compare(nameof(Password), ErrorMessage = "Password and ConfirmPassword must be equal")]
+        public string ConfirmPassword { get; set; }
+    }
+}
