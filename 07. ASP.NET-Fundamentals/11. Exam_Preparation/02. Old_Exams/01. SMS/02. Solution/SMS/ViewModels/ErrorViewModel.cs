e555b07aaac6480ea63400315984fc8c45b0d08a
mikegsCoder <mikegsid@gmail.com>
2022-11-12 12:13:13 +0200

    Add ErrorViewModel

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/01. SMS/02. Solution/SMS/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string ErrorMessage { get; set; }
+    }
+}
