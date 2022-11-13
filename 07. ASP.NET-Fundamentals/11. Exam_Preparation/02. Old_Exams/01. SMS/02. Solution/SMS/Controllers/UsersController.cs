e159f76e43f3d7b4ee9510a362b7d78c16ae9785
mikegsCoder <mikegsid@gmail.com>
2022-11-13 11:30:48 +0200

    Add UserController initial commit

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/01. SMS/02. Solution/SMS/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using MyWebServer.Controllers;
+using MyWebServer.Http;
+using SMS.Services.UserService;
+using SMS.ViewModels;
+using SMS.ViewModels.UserViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly IUserService userService;
+
+        public UsersController(
+            IUserService _userService)
+        {
+            userService = _userService;
+        }
+
+        public HttpResponse Register()
+        {
+            if (this.User.IsAuthenticated)
+            {
+                return Redirect("/Home/Index");
+            }
+
+            return View();
+        }
+    }
+}
