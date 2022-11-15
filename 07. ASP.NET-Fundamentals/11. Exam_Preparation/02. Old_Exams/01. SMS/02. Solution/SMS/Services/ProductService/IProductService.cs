a34be65e3d3efec50eb277b9898af9ea7205a911
mikegsCoder <mikegsid@gmail.com>
2022-11-15 16:59:53 +0200

    Add ProductService

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/01. SMS/02. Solution/SMS/Services/ProductService/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using SMS.ViewModels.ProductViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.Services.ProductService
+{
+    public interface IProductService
+    {
+        ICollection<string> Create(CreateProductViewModel model);
+    }
+}
---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/01. SMS/02. Solution/SMS/Services/ProductService/ProductService.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using SMS.Data.Common;
+using SMS.Data.Models;
+using SMS.Services.ValidationService;
+using SMS.ViewModels.ProductViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.Services.ProductService
+{
+    public class ProductService : IProductService
+    {   
+        private readonly IRepository repo;
+        private readonly IValidationService validationService;
+
+        public ProductService(
+            IRepository _repo,
+            IValidationService _validationService)
+        {
+            repo = _repo;
+            validationService = _validationService;
+        }
+
+        public ICollection<string> Create(CreateProductViewModel model)
+        {
+            var errors = new List<string>();
+
+            var validationErrors = validationService.ValidateModel(model);
+
+            if (validationErrors.Any())
+            {
+                return validationErrors;
+            }
+
+            decimal price = 0;
+
+            if (!decimal.TryParse(model.Price, NumberStyles.Float, CultureInfo.InvariantCulture, out price)
+                || price < 0.05M || price > 1000M)
+            {
+                errors.Add("Price must be between 0.05 and 1000");
+
+                return errors;
+            }
+
+            var product = new Product()
+            {
+                Name = model.Name,
+                Price = price
+            };
+
+            try
+            {
+                repo.Add(product);
+                repo.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                errors.Add(ex.Message);
+            }
+
+            return errors;
+        }
+    }
+}
