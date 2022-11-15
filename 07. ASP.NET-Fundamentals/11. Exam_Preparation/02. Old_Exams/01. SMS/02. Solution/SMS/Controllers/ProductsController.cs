2041427051ba3256d802c55bba0a752715c796e3
mikegsCoder <mikegsid@gmail.com>
2022-11-15 17:06:58 +0200

    Add HttpPost Create

--- 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/01. SMS/02. Solution/SMS/Controllers/ProductsController.cs
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/01. SMS/02. Solution/SMS/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using MyWebServer.Controllers;
 using MyWebServer.Http;
+using SMS.Services.ProductService;
 using SMS.ViewModels;
 using SMS.ViewModels.ProductViewModels;
 using System;
@@ -12,10 +13,36 @@ namespace SMS.Controllers
 {
     public class ProductsController : Controller
     {
+        private readonly IProductService productService;
+
+        public ProductsController(IProductService _productService)
+        {
+            productService = _productService;
+        }
+
         [Authorize]
         public HttpResponse Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        [Authorize]
+        public HttpResponse Create(CreateProductViewModel model)
+        {
+            var errors = productService.Create(model);
+
+            if (errors.Any())
+            {
+                ErrorViewModel errorModel = new ErrorViewModel()
+                {
+                    ErrorMessage = String.Join(", ", errors)
+                };
+
+                return View(errorModel, "/Error");
+            }
+
+            return Redirect("/");
         }
     }
 }
