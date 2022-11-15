b98a7a4fd3bc36bfd283f97c5d4a99f5d5dbf15f
mikegsCoder <mikegsid@gmail.com>
2022-11-15 16:57:12 +0200

    Add ProductsController initial commit

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/01. SMS/02. Solution/SMS/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using MyWebServer.Controllers;
+using MyWebServer.Http;
+using SMS.ViewModels;
+using SMS.ViewModels.ProductViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.Controllers
+{
+    public class ProductsController : Controller
+    {
+        [Authorize]
+        public HttpResponse Create()
+        {
+            return View();
+        }
+    }
+}
