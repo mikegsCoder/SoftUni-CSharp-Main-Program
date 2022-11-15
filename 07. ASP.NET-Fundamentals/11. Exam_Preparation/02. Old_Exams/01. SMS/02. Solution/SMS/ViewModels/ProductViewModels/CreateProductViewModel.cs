5a83fce01cd8d3b98ba79adb93897bfa57a43cf4
mikegsCoder <mikegsid@gmail.com>
2022-11-15 16:51:52 +0200

    Add CreateProductViewModel

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/01. SMS/02. Solution/SMS/ViewModels/ProductViewModels/CreateProductViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.ViewModels.ProductViewModels
+{
+    public class CreateProductViewModel
+    {
+        [Required]
+        [StringLength(20, MinimumLength = 4, ErrorMessage = "{0} must be between {2} and {1} characters")]
+        public string Name { get; set; }
+
+        public string Price { get; set; }
+    }
+}
