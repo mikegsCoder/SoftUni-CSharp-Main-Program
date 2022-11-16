dc4e7e84b4c6e5a29ec69656eb36facc0a1e8737
mikegsCoder <mikegsid@gmail.com>
2022-11-16 19:11:56 +0200

    Add Redirect logged in users to IndexLoggedIn

--- 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/01. SMS/02. Solution/SMS/Controllers/HomeController.cs
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/01. SMS/02. Solution/SMS/Controllers/HomeController.cs
@@ -2,11 +2,38 @@
 {
     using MyWebServer.Controllers;
     using MyWebServer.Http;
+    using SMS.Services.ProductService;
+    using SMS.Services.UserService;
+    using SMS.ViewModels.ProductViewModels;
 
     public class HomeController : Controller
     {
+        private readonly IUserService userService;
+        private readonly IProductService productService;
+
+        public HomeController(
+            IUserService _userService,
+            IProductService _productService)
+        {
+            userService = _userService;
+            productService = _productService;
+        }
+
         public HttpResponse Index()
         {
+            if (User.IsAuthenticated)
+            {
+                string username = userService.GetUsername(User.Id);
+
+                AllProductsViewModel model = new AllProductsViewModel()
+                {
+                    Username = username,
+                    Products = productService.GetProducts()
+                };
+
+                return View(model, "/Home/IndexLoggedIn");
+            }
+
             return this.View();
         }
     }
