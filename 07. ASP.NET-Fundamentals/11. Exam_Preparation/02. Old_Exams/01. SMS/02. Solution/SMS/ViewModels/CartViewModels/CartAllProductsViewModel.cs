3cc7f088ecf275660d4d4b236b2f7ad48a3f8d18
mikegsCoder <mikegsid@gmail.com>
2022-11-17 09:58:19 +0200

    Add CartAllProductsViewModel

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/01. SMS/02. Solution/SMS/ViewModels/CartViewModels/CartAllProductsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.ViewModels.CartViewModels
+{
+    public class CartAllProductsViewModel
+    {
+        public IEnumerable<CartProductViewModel> Products { get; set; }
+    }
+}
