58c2ea607c13bd8903e73c6159ffb24e928258e8
mikegsCoder <mikegsid@gmail.com>
2022-11-17 10:03:56 +0200

    Add CartsController

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/01. SMS/02. Solution/SMS/Controllers/CartsController.cs
@@ -0,0 +1,24 @@
+﻿using MyWebServer.Controllers;
+using MyWebServer.Http;
+using SMS.Services.CartService;
+
+namespace SMS.Controllers
+{
+    public class CartsController : Controller
+    {
+        private readonly ICartService cartService;
+
+        public CartsController(ICartService _cartService) 
+        {
+            cartService = _cartService;
+        }
+
+        [Authorize]
+        public HttpResponse AddProduct(string productId)
+        {
+            var products = cartService.AddProduct(productId, User.Id);
+
+            return View(products, "/Carts/Details");
+        }
+    }
+}
