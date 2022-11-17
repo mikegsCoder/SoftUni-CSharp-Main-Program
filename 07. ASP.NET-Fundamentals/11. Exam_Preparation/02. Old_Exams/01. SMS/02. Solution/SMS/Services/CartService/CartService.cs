f5ddf2ae67fc21bed023a93f462c71375d4d2312
mikegsCoder <mikegsid@gmail.com>
2022-11-17 10:00:32 +0200

    Add CartService

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/01. SMS/02. Solution/SMS/Services/CartService/CartService.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SMS.Data.Common;
+using SMS.Data.Models;
+using SMS.ViewModels.CartViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.Services.CartService
+{
+    public class CartService : ICartService
+    {
+        private readonly IRepository repo;
+
+        public CartService(IRepository _repo)
+        {
+            this.repo = _repo;
+        }
+
+        public CartAllProductsViewModel AddProduct(string productId, string userId)
+        {
+            var user = repo.All<User>()
+                .Where(u => u.Id == userId)
+                .Include(u => u.Cart)
+                .ThenInclude(c => c.Products)
+                .FirstOrDefault();
+
+            var product = repo.All<Product>()
+                .FirstOrDefault(p => p.Id == productId);
+
+            user.Cart.Products.Add(product);
+
+            try
+            {
+                repo.SaveChanges();
+            }
+            catch (Exception)
+            { }
+
+            CartAllProductsViewModel model = new CartAllProductsViewModel()
+            {
+                Products = user
+                .Cart
+                .Products
+                .Select(p => new CartProductViewModel()
+                {
+                    ProductName = p.Name,
+                    ProductPrice = p.Price.ToString("F2")
+                })
+            };
+
+            return model;
+        }
+    }
+}
---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/01. SMS/02. Solution/SMS/Services/CartService/ICartService.cs
@@ -0,0 +1,15 @@
+﻿using SMS.Data.Models;
+using SMS.ViewModels.CartViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.Services.CartService
+{
+    public interface ICartService
+    {
+        CartAllProductsViewModel AddProduct(string productId, string userId);
+    }
+}
