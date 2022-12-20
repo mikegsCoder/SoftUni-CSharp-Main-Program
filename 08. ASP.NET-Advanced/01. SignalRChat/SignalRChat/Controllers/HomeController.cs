aae91706858ee35c84eb9cdfea6de970e17e3f32
mikegsCoder <mikegsid@gmail.com>
2022-12-20 17:51:11 +0200

    Add SignalRChat initial commit

---
+++ 08. ASP.NET-Advanced/01. SignalRChat/SignalRChat/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SignalRChat.Models;
+using System.Diagnostics;
+
+namespace SignalRChat.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+         public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
