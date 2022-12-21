90eb6a6ee51a27a972318ef2bff2a30ec2d5e15a
mikegsCoder <mikegsid@gmail.com>
2022-12-21 17:01:46 +0200

    Add SignalR

--- 08. ASP.NET-Advanced/01. SignalRChat/SignalRChat/Program.cs
+++ 08. ASP.NET-Advanced/01. SignalRChat/SignalRChat/Program.cs
@@ -1,7 +1,10 @@
+using SignalRChat.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSignalR();
 
 var app = builder.Build();
 
@@ -23,5 +26,6 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapHub<ChatHub>("/chathub");
 
 app.Run();
