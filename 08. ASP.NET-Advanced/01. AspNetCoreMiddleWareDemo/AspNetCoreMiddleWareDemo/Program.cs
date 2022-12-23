dbfa0bd5ee98d2ffdb8cf02d106112f28e204862
mikegsCoder <mikegsid@gmail.com>
2022-12-23 10:16:04 +0200

    Add CreateHostBuilder

--- 08. ASP.NET-Advanced/01. AspNetCoreMiddleWareDemo/AspNetCoreMiddleWareDemo/Program.cs
+++ 08. ASP.NET-Advanced/01. AspNetCoreMiddleWareDemo/AspNetCoreMiddleWareDemo/Program.cs
@@ -13,6 +13,14 @@ namespace AspNetCoreMiddleWareDemo
     {
         public static void Main(string[] args)
         {
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+           Host.CreateDefaultBuilder(args)
+               .ConfigureWebHostDefaults(webBuilder =>
+               {
+                   webBuilder.UseStartup<Startup>();
+               });
     }
 }
