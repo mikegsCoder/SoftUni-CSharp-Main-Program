f7cd7fac797c8ba321215758e364f5043fac51be
mikegsCoder <mikegsid@gmail.com>
2022-12-23 10:11:02 +0200

    Add AspNetCoreMiddlewareDemo initial commit

---
+++ 08. ASP.NET-Advanced/01. AspNetCoreMiddleWareDemo/AspNetCoreMiddleWareDemo/Program.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreMiddleWareDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
