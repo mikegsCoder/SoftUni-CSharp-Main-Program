2a99f7d366f18ce41470abc78262daee65c79848
mikegsCoder <mikegsid@gmail.com>
2022-12-26 12:44:35 +0200

    Add Agent Db model

---
+++ 08. ASP.NET-Advanced/04. HouseRentingSystem/HouseRentingSystem.Services/Data/Entities/Agent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+using static HouseRentingSystem.Services.Data.DataConstants.Agent;
+
+namespace HouseRentingSystem.Services.Data.Entities
+{
+    public class Agent
+    {
+        public int Id { get; init; }
+
+        [Required]
+        [MaxLength(PhoneNumberMaxLength)]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        public User User { get; init; }
+    }
+}
