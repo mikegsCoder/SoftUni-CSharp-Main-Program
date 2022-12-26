0b2bc20c9a4b96dc21939c55060cb79fe4703be7
mikegsCoder <mikegsid@gmail.com>
2022-12-26 12:39:37 +0200

    Add Category Db Model

---
+++ 08. ASP.NET-Advanced/04. HouseRentingSystem/HouseRentingSystem.Services/Data/Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static HouseRentingSystem.Services.Data.DataConstants.Category;
+
+namespace HouseRentingSystem.Services.Data.Entities
+{
+    public class Category
+    {
+        public int Id { get; init; }
+
+        [Required]
+        [MaxLength(NameMaxLength)]
+        public string Name { get; set; }
+
+        public IEnumerable<House> Houses { get; init; } = new List<House>();
+    }
+}
