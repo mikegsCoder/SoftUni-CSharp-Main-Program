f91006096e6abc5cc87b625494469a955cefd20e
mikegsCoder <mikegsid@gmail.com>
2022-12-26 12:36:02 +0200

    Add DataConstants

---
+++ 08. ASP.NET-Advanced/04. HouseRentingSystem/HouseRentingSystem.Services/Data/DataConstants.cs
@@ -0,0 +1,16 @@
+﻿namespace HouseRentingSystem.Services.Data
+{
+    public class DataConstants
+    {
+        public class User
+        {
+            //First name
+            public const int UserFirstNameMaxLength = 12;
+            public const int UserFirstNameMinLength = 1;
+
+            //Last name
+            public const int UserLastNameMaxLength = 15;
+            public const int UserLastNameMinLength = 3;
+        }
+    }
+}
