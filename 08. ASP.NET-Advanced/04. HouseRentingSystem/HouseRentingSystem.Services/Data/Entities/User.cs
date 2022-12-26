b790a46135029e193d9788d6c632f2dec1318469
mikegsCoder <mikegsid@gmail.com>
2022-12-26 12:37:02 +0200

    Add User Db model

---
+++ 08. ASP.NET-Advanced/04. HouseRentingSystem/HouseRentingSystem.Services/Data/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using static HouseRentingSystem.Services.Data.DataConstants.User;
+
+namespace HouseRentingSystem.Services.Data.Entities
+{
+    public class User : IdentityUser
+    {
+        [Required]
+        [MaxLength(UserFirstNameMaxLength)]
+        public string FirstName { get; init; }
+
+        [Required]
+        [MaxLength(UserLastNameMaxLength)]
+        public string LastName { get; init; }
+    }
+}
