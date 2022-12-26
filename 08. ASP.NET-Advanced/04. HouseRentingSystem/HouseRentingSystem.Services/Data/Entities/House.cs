ad15c7e446ca32d76630be4ce4b316c757e839af
mikegsCoder <mikegsid@gmail.com>
2022-12-26 12:42:03 +0200

    Add House Db model

---
+++ 08. ASP.NET-Advanced/04. HouseRentingSystem/HouseRentingSystem.Services/Data/Entities/House.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using static HouseRentingSystem.Services.Data.DataConstants.House;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace HouseRentingSystem.Services.Data.Entities
+{
+    public class House
+    {
+        public int Id { get; init; }
+
+        [Required]
+        [MaxLength(TitleMaxLength)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(AddressMaxLength)]
+        public string Address { get; set; }
+
+        [Required]
+        [MaxLength(DescriptionMaxLength)]
+        public string Description { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+
+        [Column(TypeName = "decimal(12,3)")]
+        public decimal PricePerMonth { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public Category Category { get; init; }
+
+        public int AgentId { get; set; }
+
+        public Agent Agent { get; init; }
+
+        public string? RenterId { get; set; }
+    }
+}
