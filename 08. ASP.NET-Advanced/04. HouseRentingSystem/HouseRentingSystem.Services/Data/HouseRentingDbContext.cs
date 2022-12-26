be803a5c5f3864c09e848472496705facda289c3
mikegsCoder <mikegsid@gmail.com>
2022-12-26 12:34:04 +0200

    Add DbContext

---
+++ 08. ASP.NET-Advanced/04. HouseRentingSystem/HouseRentingSystem.Services/Data/HouseRentingDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HouseRentingSystem.Services.Data
+{
+    public class HouseRentingDbContext : IdentityDbContext<IdentityUser>
+    {
+        public HouseRentingDbContext
+            (DbContextOptions<HouseRentingDbContext> options)
+            : base(options)
+        {
+            this.Database.Migrate();
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            
+        }
+    }
+}
+
+
