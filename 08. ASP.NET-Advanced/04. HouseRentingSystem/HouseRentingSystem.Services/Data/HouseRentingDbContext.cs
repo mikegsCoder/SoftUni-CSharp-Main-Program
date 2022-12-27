98be39586b26eaf76056d071afacc29dd2783edf
mikegsCoder <mikegsid@gmail.com>
2022-12-27 16:38:11 +0200

    Add relations in Db context

--- 08. ASP.NET-Advanced/04. HouseRentingSystem/HouseRentingSystem.Services/Data/HouseRentingDbContext.cs
+++ 08. ASP.NET-Advanced/04. HouseRentingSystem/HouseRentingSystem.Services/Data/HouseRentingDbContext.cs
@@ -22,7 +22,19 @@ namespace HouseRentingSystem.Services.Data
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            
+            builder
+               .Entity<House>()
+               .HasOne(h => h.Category)
+               .WithMany(c => c.Houses)
+               .HasForeignKey(h => h.CategoryId)
+               .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+               .Entity<House>()
+               .HasOne(h => h.Agent)
+               .WithMany()
+               .HasForeignKey(h => h.AgentId)
+               .OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
