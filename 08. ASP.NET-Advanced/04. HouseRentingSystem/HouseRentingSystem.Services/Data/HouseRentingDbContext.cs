6c7f9af73c442c2d9a5f0701720c2f2659ceebb4
mikegsCoder <mikegsid@gmail.com>
2022-12-27 16:39:22 +0200

    Add private fields

--- 08. ASP.NET-Advanced/04. HouseRentingSystem/HouseRentingSystem.Services/Data/HouseRentingDbContext.cs
+++ 08. ASP.NET-Advanced/04. HouseRentingSystem/HouseRentingSystem.Services/Data/HouseRentingDbContext.cs
@@ -9,6 +9,19 @@ namespace HouseRentingSystem.Services.Data
 {
     public class HouseRentingDbContext : IdentityDbContext<User>
     {
+        private User AdminUser { get; set; }
+        private Agent AdminAgent { get; set; }
+
+        private User AgentUser { get; set; }
+        private User GuestUser { get; set; }
+        private Agent Agent { get; set; }
+        private Category CottageCategory { get; set; }
+        private Category SingleCategory { get; set; }
+        private Category DuplexCategory { get; set; }
+        private House FirstHouse { get; set; }
+        private House SecondHouse { get; set; }
+        private House ThirdHouse { get; set; }
+
         public HouseRentingDbContext
             (DbContextOptions<HouseRentingDbContext> options)
             : base(options)
