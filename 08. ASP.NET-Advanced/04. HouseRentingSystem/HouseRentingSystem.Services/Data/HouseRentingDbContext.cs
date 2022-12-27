9dca8e6daef90963ebff114b62f18e98bde8384c
mikegsCoder <mikegsid@gmail.com>
2022-12-27 16:46:41 +0200

    Add SeedHouses

--- 08. ASP.NET-Advanced/04. HouseRentingSystem/HouseRentingSystem.Services/Data/HouseRentingDbContext.cs
+++ 08. ASP.NET-Advanced/04. HouseRentingSystem/HouseRentingSystem.Services/Data/HouseRentingDbContext.cs
@@ -65,6 +65,14 @@ namespace HouseRentingSystem.Services.Data
                 .HasData(this.CottageCategory,
                         this.SingleCategory,
                         this.DuplexCategory);
+
+            SeedHouses();
+            builder.Entity<House>()
+                .HasData(this.FirstHouse,
+                        this.SecondHouse,
+                        this.ThirdHouse);
+
+            base.OnModelCreating(builder);
         }
 
         private void SeedUsers()
@@ -149,6 +157,47 @@ namespace HouseRentingSystem.Services.Data
             {
                 Id = 3,
                 Name = "Duplex"
+            };
+        }
+
+        private void SeedHouses()
+        {
+            this.FirstHouse = new House()
+            {
+                Id = 1,
+                Title = "Big House Marina",
+                Address = "North London, UK (near the border)",
+                Description = "A big house for your whole family. Don't miss to buy a house with three bedrooms.",
+                ImageUrl = "https://www.luxury-architecture.net/wp-content/uploads/2017/12/1513217889-7597-FAIRWAYS-010.jpg",
+                PricePerMonth = 2100.00M,
+                CategoryId = this.DuplexCategory.Id,
+                AgentId = this.Agent.Id,
+                RenterId = this.GuestUser.Id
+            };
+
+            this.SecondHouse = new House()
+            {
+                Id = 2,
+                Title = "Family House Comfort",
+                Address = "Near the Sea Garden in Burgas, Bulgaria",
+                Description = "It has the best comfort you will ever ask for. With two bedrooms, it is great for your family.",
+                ImageUrl = "https://cf.bstatic.com/xdata/images/hotel/max1024x768/179489660.jpg" +
+                "?k=2029f6d9589b49c95dcc9503a265e292c2cdfcb5277487a0050397c3f8dd545a&o=&hp=1",
+                PricePerMonth = 1200.00M,
+                CategoryId = this.SingleCategory.Id,
+                AgentId = this.Agent.Id
+            };
+
+            this.ThirdHouse = new House()
+            {
+                Id = 3,
+                Title = "Grand House",
+                Address = "Boyana Neighbourhood, Sofia, Bulgaria",
+                Description = "This luxurious house is everything you will need. It is just excellent.",
+                ImageUrl = "https://i.pinimg.com/originals/a6/f5/85/a6f5850a77633c56e4e4ac4f867e3c00.jpg",
+                PricePerMonth = 2000.00M,
+                CategoryId = this.SingleCategory.Id,
+                AgentId = this.Agent.Id
             };
         }
     }
