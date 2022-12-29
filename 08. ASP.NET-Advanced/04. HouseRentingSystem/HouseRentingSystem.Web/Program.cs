1f0ce512fe516475c974ef1cf1a1077cd3367746
mikegsCoder <mikegsid@gmail.com>
2022-12-29 12:52:14 +0200

    Services configuration

--- 08. ASP.NET-Advanced/04. HouseRentingSystem/HouseRentingSystem.Web/Program.cs
+++ 08. ASP.NET-Advanced/04. HouseRentingSystem/HouseRentingSystem.Web/Program.cs
@@ -1,18 +1,44 @@
+using HouseRentingSystem.Services.Data;
+using HouseRentingSystem.Services.Data.Entities;
+using HouseRentingSystem.Services.Houses;
+using HouseRentingSystem.Web.Controllers;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
+builder.Services.AddDbContext<HouseRentingSystem.Services.Data.HouseRentingDbContext>(options =>
+    options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
+builder.Services.AddDefaultIdentity<User>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+})
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<HouseRentingDbContext>();
 
 builder.Services.AddControllersWithViews(options =>
 {
     options.Filters.Add<AutoValidateAntiforgeryTokenAttribute>();
 });
 
+builder.Services.AddAutoMapper(
+    typeof(IHouseService).Assembly,
+    typeof(HomeController).Assembly);
+
+builder.Services.AddControllersWithViews(options =>
+{
+    options.Filters.Add<AutoValidateAntiforgeryTokenAttribute>();
+});
+
+builder.Services.AddTransient<IHouseService, HouseService>();
 
 var app = builder.Build();
 
