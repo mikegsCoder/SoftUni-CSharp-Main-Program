6bd7c23e80cc17740674119fb244cee5aa252b3b
mikegsCoder <mikegsid@gmail.com>
2022-12-29 13:29:09 +0200

    Add HouseFormModel

---
+++ 08. ASP.NET-Advanced/04. HouseRentingSystem/HouseRentingSystem.Web/Models/Houses/HouseFormModel.cs
@@ -0,0 +1,40 @@
+﻿using HouseRentingSystem.Services.Houses.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+using static HouseRentingSystem.Services.Data.DataConstants.House;
+
+namespace HouseRentingSystem.Web.Models.Houses
+{
+    public class HouseFormModel : IHouseModel
+    {
+
+        [Required]
+        [StringLength(TitleMaxLength, MinimumLength = TitleMinLength)]
+        public string Title { get; init; }
+
+        [Required]
+        [StringLength(AddressMaxLength, MinimumLength = AddressMinLength)]
+        public string Address { get; init; }
+
+        [Required]
+        [StringLength(DescriptionMaxLength, MinimumLength = DescriptionMinLength)]
+        public string Description { get; init; }
+
+        [Required]
+        [Display(Name = "Image URL")]
+        public string ImageUrl { get; init; }
+
+        [Required]
+        [Range(0.00, MaxPricePerMonth,
+            ErrorMessage = "Price Per Month must be a positive number and less than {2} leva.")]
+        [Display(Name = "Price Per Month")]
+        public decimal PricePerMonth { get; init; }
+
+        [Display(Name = "Category")]
+        public int CategoryId { get; set; }
+
+        public IEnumerable<HouseCategoryServiceModel> Categories { get; set; }
+            = new List<HouseCategoryServiceModel>();
+    }
+}
