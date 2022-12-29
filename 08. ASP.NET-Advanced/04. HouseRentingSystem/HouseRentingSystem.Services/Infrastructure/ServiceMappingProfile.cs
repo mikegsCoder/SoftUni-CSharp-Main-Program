12524390a25e6584694fc0ab6fc0b86826ad9b11
mikegsCoder <mikegsid@gmail.com>
2022-12-29 13:21:45 +0200

    Add ServiceMappingProfile

---
+++ 08. ASP.NET-Advanced/04. HouseRentingSystem/HouseRentingSystem.Services/Infrastructure/ServiceMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using HouseRentingSystem.Services.Data.Entities;
+using HouseRentingSystem.Services.Houses.Models;
+
+namespace HouseRentingSystem.Services.Infrastructure
+{
+    public class ServiceMappingProfile : Profile
+    {
+        public ServiceMappingProfile()
+        {
+            this.CreateMap<House, HouseIndexServiceModel>();
+        }
+    }
+}
+
+
