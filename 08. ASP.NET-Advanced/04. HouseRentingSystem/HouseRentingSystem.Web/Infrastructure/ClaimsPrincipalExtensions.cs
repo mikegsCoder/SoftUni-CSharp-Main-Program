62f7d96af1780530e7bc988cf30dcf4f0a94de85
mikegsCoder <mikegsid@gmail.com>
2022-12-29 13:25:44 +0200

    Add ClaimsPrincipalExtensions

---
+++ 08. ASP.NET-Advanced/04. HouseRentingSystem/HouseRentingSystem.Web/Infrastructure/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Claims;
+using static HouseRentingSystem.Web.AdminConstants;
+
+namespace HouseRentingSystem.Web.Infrastructure
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string Id(this ClaimsPrincipal user)
+            => user.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+        public static bool IsAdmin(this ClaimsPrincipal user)
+           => user.IsInRole(AdminRoleName);
+    }
+}
