af480c6500ebe745b1b5de9b8b89cab4f8f0a150
mikegsCoder <mikegsid@gmail.com>
2022-12-29 13:30:22 +0200

    Add HouseDetailsViewModel

---
+++ 08. ASP.NET-Advanced/04. HouseRentingSystem/HouseRentingSystem.Web/Models/Houses/HouseDetailsViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace HouseRentingSystem.Web.Models.Houses
+{
+    public class HouseDetailsViewModel
+    {
+        public int Id { get; init; }
+
+        public string Title { get; init; }
+
+        public string Address { get; init; }
+
+        public string ImageUrl { get; init; }
+    }
+}
