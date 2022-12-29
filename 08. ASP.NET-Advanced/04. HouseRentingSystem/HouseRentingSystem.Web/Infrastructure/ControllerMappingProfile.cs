d1e2561e75c48f08ab96c8bf7646d2874987d865
mikegsCoder <mikegsid@gmail.com>
2022-12-29 13:27:15 +0200

    Add ControllerMappingProfile

---
+++ 08. ASP.NET-Advanced/04. HouseRentingSystem/HouseRentingSystem.Web/Infrastructure/ControllerMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using HouseRentingSystem.Services.Houses.Models;
+using HouseRentingSystem.Web.Models.Houses;
+
+namespace HouseRentingSystem.Web.Infrastructure
+{
+    public class ControllerMappingProfile
+        : Profile
+    {
+        public ControllerMappingProfile()
+        {
+            this.CreateMap<HouseDetailsServiceModel, HouseFormModel>();
+            this.CreateMap<HouseDetailsServiceModel, HouseDetailsViewModel>();
+        }
+    }
+}
