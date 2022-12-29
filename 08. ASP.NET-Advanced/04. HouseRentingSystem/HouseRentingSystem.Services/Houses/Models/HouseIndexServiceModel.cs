04f385b8fc35ffbd625faed933e4650b2c4d1907
mikegsCoder <mikegsid@gmail.com>
2022-12-29 12:38:50 +0200

    Add HouseIndexServiceModel

---
+++ 08. ASP.NET-Advanced/04. HouseRentingSystem/HouseRentingSystem.Services/Houses/Models/HouseIndexServiceModel.cs
@@ -0,0 +1,10 @@
+﻿namespace HouseRentingSystem.Services.Houses.Models
+{
+    public class HouseIndexServiceModel : IHouseModel
+    {
+        public string Address { get; init; }
+        public int Id { get; init; }
+        public string Title { get; init; }
+        public string ImageUrl { get; init; }
+    }
+}
---
+++ 08. ASP.NET-Advanced/04. HouseRentingSystem/HouseRentingSystem.Services/Houses/Models/IHouseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HouseRentingSystem.Services.Houses.Models
+{
+    public interface IHouseModel
+    {
+        public string Title { get; }
+
+        public string Address { get; }
+    }
+}
