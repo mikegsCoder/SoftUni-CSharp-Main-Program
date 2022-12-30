88900ecd560c064247f6d112c5b90cc5c74ecc88
mikegsCoder <mikegsid@gmail.com>
2022-12-30 11:45:18 +0200

    Work around houses

---
+++ 08. ASP.NET-Advanced/04. HouseRentingSystem/HouseRentingSystem.Services/Houses/Models/HouseCategoryServiceModel.cs
@@ -0,0 +1,8 @@
+﻿namespace HouseRentingSystem.Services.Houses.Models
+{
+    public class HouseCategoryServiceModel
+    {
+        public int Id { get; init; }
+        public string Name { get; init; }
+    }
+}
---
+++ 08. ASP.NET-Advanced/04. HouseRentingSystem/HouseRentingSystem.Web/AdminConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace HouseRentingSystem.Web
+{
+    public class AdminConstants
+    {
+        public const string AdminRoleName = "Administrator";
+        public const string AdminEmail = "admin@mail.com";
+    }
+}
