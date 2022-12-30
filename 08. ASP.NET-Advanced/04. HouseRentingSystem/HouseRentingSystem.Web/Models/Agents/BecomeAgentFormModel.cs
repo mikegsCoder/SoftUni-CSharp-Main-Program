2fb96b3370ff7bad53d862f0c1e4aaa91ec2ae77
mikegsCoder <mikegsid@gmail.com>
2022-12-30 11:05:44 +0200

    Add view model

---
+++ 08. ASP.NET-Advanced/04. HouseRentingSystem/HouseRentingSystem.Web/Models/Agents/BecomeAgentFormModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using static HouseRentingSystem.Services.Data.DataConstants.Agent;
+
+namespace HouseRentingSystem.Web.Models.Agents
+{
+    public class BecomeAgentFormModel
+    {
+        [Required]
+        [StringLength(PhoneNumberMaxLength, MinimumLength = PhoneNumberMinLength)]
+        [Phone]
+        public string PhoneNumber { get; init; }
+    }
+}
