66f0ee1d7587f8ae2773b3ac2e19d21e370ad8c4
mikegsCoder <mikegsid@gmail.com>
2022-12-30 11:01:44 +0200

    Add AgentsController

---
+++ 08. ASP.NET-Advanced/04. HouseRentingSystem/HouseRentingSystem.Web/Controllers/AgentsController.cs
@@ -0,0 +1,31 @@
+﻿using HouseRentingSystem.Web.Infrastructure;
+using HouseRentingSystem.Web.Models.Agents;
+using HouseRentingSystem.Services.Agents;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouseRentingSystem.Web.Controllers
+{
+    public class AgentsController : Controller
+    {
+        private readonly IAgentService agents;
+
+        public AgentsController(IAgentService agents)
+        {
+            this.agents = agents;
+        }
+
+
+        [Authorize]
+        public IActionResult Become()
+        {
+            if (this.agents.ExistsById(this.User.Id()))
+            {
+                return BadRequest();
+            }
+            return View();
+        }
+
+
+}
+
