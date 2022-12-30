a47a9e4a76c1a2bbfd0ff08e565ffd7bf0ee439f
mikegsCoder <mikegsid@gmail.com>
2022-12-30 10:57:48 +0200

    Add HouseDetailsServiceModel

---
+++ 08. ASP.NET-Advanced/04. HouseRentingSystem/HouseRentingSystem.Services/Houses/Models/HouseDetailsServiceModel.cs
@@ -0,0 +1,15 @@
+﻿using HouseRentingSystem.Services.Agents.Models;
+
+namespace HouseRentingSystem.Services.Houses.Models
+{
+    public class HouseDetailsServiceModel : HouseServiceModel
+    {
+        public string Description { get; init; }
+
+        public string Category { get; init; }
+
+        public AgentServiceModel Agent { get; set; }
+    }
+}
+
+
