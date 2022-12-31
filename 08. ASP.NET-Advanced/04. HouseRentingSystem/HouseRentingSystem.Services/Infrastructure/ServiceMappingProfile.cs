011fed1c31c69c50a5b19420f71c759a24d3b572
mikegsCoder <mikegsid@gmail.com>
2022-12-31 12:52:28 +0200

    Add maps

--- 08. ASP.NET-Advanced/04. HouseRentingSystem/HouseRentingSystem.Services/Infrastructure/ServiceMappingProfile.cs
+++ 08. ASP.NET-Advanced/04. HouseRentingSystem/HouseRentingSystem.Services/Infrastructure/ServiceMappingProfile.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using HouseRentingSystem.Services.Agents.Models;
 using HouseRentingSystem.Services.Data.Entities;
 using HouseRentingSystem.Services.Houses.Models;
 
@@ -9,6 +10,19 @@ namespace HouseRentingSystem.Services.Infrastructure
         public ServiceMappingProfile()
         {
             this.CreateMap<House, HouseIndexServiceModel>();
+
+            this.CreateMap<House, HouseServiceModel>()
+                .ForMember(h => h.IsRented, cfg => cfg.MapFrom(h => h.RenterId != null));
+
+            this.CreateMap<House, HouseDetailsServiceModel>()
+                .ForMember(h => h.IsRented, cfg => cfg.MapFrom(h => h.RenterId != null))
+                .ForMember(h => h.Category, cfg => cfg.MapFrom(h => h.Category.Name));
+
+            this.CreateMap<Agent, AgentServiceModel>()
+                .ForMember(ag => ag.Email, cfg => cfg
+                    .MapFrom(ag => ag.User.Email));
+
+            this.CreateMap<Category, HouseCategoryServiceModel>();
         }
     }
 }
