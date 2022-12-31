682e2620bc9b63daa9b0653efea61a545e6f7f33
mikegsCoder <mikegsid@gmail.com>
2022-12-31 12:38:32 +0200

    Add ApplicationBuilderExtensions

---
+++ 08. ASP.NET-Advanced/04. HouseRentingSystem/HouseRentingSystem.Web/Infrastructure/ApplicationBuilderExtensions.cs
@@ -0,0 +1,43 @@
+﻿using HouseRentingSystem.Services.Data.Entities;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+using static HouseRentingSystem.Web.AdminConstants;
+
+namespace HouseRentingSystem.Web.Infrastructure
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder SeedAdmin(
+            this IApplicationBuilder app)
+        {
+            using var scopedServices = app.ApplicationServices.CreateScope();
+            var services = scopedServices.ServiceProvider;
+
+            var userManager = services.GetRequiredService<UserManager<User>>();
+            var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+            Task
+                .Run(async () =>
+                {
+                    if (await roleManager.RoleExistsAsync(AdminRoleName))
+                    {
+                        return;
+                    }
+
+                    var role = new IdentityRole { Name = AdminRoleName };
+
+                    await roleManager.CreateAsync(role);
+
+                    var admin = await userManager.FindByNameAsync(AdminEmail);
+
+                    await userManager.AddToRoleAsync(admin, role.Name);
+                })
+                .GetAwaiter()
+                .GetResult();
+
+            return app;
+        }
+    }
+}
