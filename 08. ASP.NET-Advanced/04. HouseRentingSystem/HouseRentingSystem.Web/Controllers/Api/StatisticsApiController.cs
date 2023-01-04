53893d3f3d2e2d516dfc5af51fa746bbd9a29a01
mikegsCoder <mikegsid@gmail.com>
2023-01-04 16:46:28 +0200

    Add StatisticsApiController

---
+++ 08. ASP.NET-Advanced/04. HouseRentingSystem/HouseRentingSystem.Web/Controllers/Api/StatisticsApiController.cs
@@ -0,0 +1,21 @@
+﻿using HouseRentingSystem.Services.Statistics;
+using HouseRentingSystem.Services.Statistics.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouseRentingSystem.Web.Controllers.Api
+{
+    [ApiController]
+    [Route("api/statistics")]
+    public class StatisticsApiController : ControllerBase
+    {
+        private readonly IStatisticsService statistics;
+
+        public StatisticsApiController(IStatisticsService statistics)
+            => this.statistics = statistics;
+
+        [HttpGet]
+        public StatisticsServiceModel GetStatistics()
+            => this.statistics.Total();
+    }
+}
+
