6cd03042a51478ad630e8f587c4054b90f8cf355
mikegsCoder <mikegsid@gmail.com>
2023-01-05 17:52:10 +0200

    Add initial commit

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/02. CarShop/02. Solution/Apps/CarShop/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using SUS.HTTP;
+using SUS.MvcFramework;
+
+using System.Collections.Generic;
+
+namespace CarShop
+{
+    public class Startup : IMvcApplication
+    {
+        public void Configure(List<Route> routeTable)
+        {
+        }
+
+        public void ConfigureServices(IServiceCollection serviceCollection)
+        {
+        }
+    }
+}
