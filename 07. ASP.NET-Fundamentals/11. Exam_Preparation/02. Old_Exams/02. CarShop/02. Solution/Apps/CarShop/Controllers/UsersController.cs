43ffcb14ccd080054e5358ae88bd38a507804c32
mikegsCoder <mikegsid@gmail.com>
2023-01-06 18:04:01 +0200

    Add UsersController

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/02. CarShop/02. Solution/Apps/CarShop/Controllers/UsersController.cs
@@ -0,0 +1,19 @@
+﻿using SUS.HTTP;
+using SUS.MvcFramework;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarShop.Controllers
+{
+    public class UsersController : Controller
+    {
+        public HttpResponse Login()
+        {
+            if (this.IsUserSignedIn())
+            {
+                return this.Redirect("/");
+            }
+
+            return this.View();
+        }
+    }
+}
