10e7007d0068e57f32bb83e60bd65e7608d6e112
mikegsCoder <mikegsid@gmail.com>
2023-01-08 11:15:13 +0200

    Add HttpPost Register

--- 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/02. CarShop/02. Solution/Apps/CarShop/Controllers/UsersController.cs
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/02. CarShop/02. Solution/Apps/CarShop/Controllers/UsersController.cs
@@ -54,5 +54,53 @@ namespace CarShop.Controllers
 
             return this.Redirect("/Cars/All");
         }
+
+        [HttpPost]
+        public HttpResponse Register(RegisterInputModel register)
+        {
+            if (this.IsUserSignedIn())
+            {
+                return this.Redirect("/");
+            }
+
+            if (string.IsNullOrEmpty(register.Username)
+                || register.Username.Length < 4
+                || register.Username.Length > 20)
+            {
+                return this.Error("Name should be between 4 and 20 characters");
+            }
+
+            if (string.IsNullOrEmpty(register.Email)
+                || !new EmailAddressAttribute().IsValid(register.Email))
+            {
+                return this.Error("Email is required");
+            }
+
+            if (string.IsNullOrEmpty(register.Password)
+                || register.Password.Length < 5
+                || register.Password.Length > 20)
+            {
+                return this.Error("Password should be between 6 and 20 characters");
+            }
+
+            if (!this.usersService.IsUsernameAvailable(register))
+            {
+                return this.Error("Username not available");
+            }
+
+            if (!this.usersService.IsEmailAvailable(register))
+            {
+                return this.Error("Email not available");
+            }
+
+            if (register.ConfirmPassword != register.Password)
+            {
+                return this.Error("Passwords do not match");
+            }
+
+            this.usersService.Create(register);
+
+            return this.Redirect("/Users/Login");
+        }
     }
 }
