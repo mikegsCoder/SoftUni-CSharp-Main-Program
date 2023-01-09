5b972e9aec6bfad58a4587c5f92d0d41f34db84a
mikegsCoder <mikegsid@gmail.com>
2023-01-09 17:31:48 +0200

    Add CarsController

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/02. CarShop/02. Solution/Apps/CarShop/Controllers/CarsController.cs
@@ -0,0 +1,45 @@
+﻿using CarShop.Services;
+using CarShop.Services.Cars;
+using CarShop.ViewModels;
+using SUS.HTTP;
+using SUS.MvcFramework;
+using System.Text.RegularExpressions;
+
+namespace CarShop.Controllers
+{
+    public class CarsController : Controller
+    {
+        private readonly ICarsService carsService;
+        private readonly IUsersService usersService;
+
+        public CarsController(
+            ICarsService carService,
+            IUsersService usersService)
+        {
+            this.carsService = carService;
+            this.usersService = usersService;
+        }
+
+        public HttpResponse All()
+        {
+            if (!this.IsUserSignedIn())
+            {
+                return this.Redirect("/");
+            }
+
+            string userId = this.GetUserId();
+            var isUserMechanic = this.usersService.IsUserMechanic(userId);
+
+            if (isUserMechanic)
+            {
+                var viewModel = this.carsService.GetAllCarsForNechanics();
+                return this.View(viewModel);
+            }
+            else
+            {
+                var viewModel = this.carsService.GetAllCars(userId);
+                return this.View(viewModel);
+            }
+        }
+    }
+}
