ba04538d766c6cc35f5d57f901f441020cc4aa75
mikegsCoder <mikegsid@gmail.com>
2023-01-11 17:20:56 +0200

    Add IssuesController

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/02. CarShop/02. Solution/Apps/CarShop/Controllers/IssuesController.cs
@@ -0,0 +1,24 @@
+﻿using SUS.HTTP;
+using SUS.MvcFramework;
+
+
+namespace CarShop.Controllers
+{
+    public class IssuesController : Controller
+    {
+        public HttpResponse Add(string carId)
+        {
+            if (!this.IsUserSignedIn())
+            {
+                return this.Redirect("/");
+            }
+
+            var viewModel = new IssueAddViewModel
+            {
+                CarId = carId
+            };
+
+            return this.View(viewModel);
+        }
+    }
+}
