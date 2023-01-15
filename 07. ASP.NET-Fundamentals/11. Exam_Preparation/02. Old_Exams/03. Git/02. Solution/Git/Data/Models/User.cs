2bc30d6740d4c2ba22d0b8f2b6d1232c50b0aede
mikegsCoder <mikegsid@gmail.com>
2023-01-15 14:27:56 +0200

    Add User Db model

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/03. Git/02. Solution/Git/Data/Models/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git.Data.Models
+{
+    public class User
+    {
+        [Key]
+        [StringLength(36)]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        [Required]
+        [StringLength(20)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(64)]
+        public string Password { get; set; }
+
+        public ICollection<Repository> Repositories { get; set; } = new List<Repository>();
+        
+        public ICollection<Commit> Commits { get; set; } = new List<Commit>();
+    }
+}
