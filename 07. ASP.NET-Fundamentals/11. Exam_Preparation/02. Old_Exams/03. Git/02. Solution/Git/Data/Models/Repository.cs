f85ee5cf19b25ea43a37a945cc7caafb23bd51f4
mikegsCoder <mikegsid@gmail.com>
2023-01-15 14:29:04 +0200

    Add Repository Db model

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/03. Git/02. Solution/Git/Data/Models/Repository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git.Data.Models
+{
+    public class Repository
+    {
+        [Key]
+        [StringLength(36)]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        [Required]
+        [StringLength(10)]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime CreatedOn { get; set; }
+
+        [Required]
+        public bool IsPublic { get; set; }
+
+        [ForeignKey(nameof(Owner))]
+        [StringLength(36)]
+        public string OwnerId { get; set; }
+        public User Owner { get; set; }
+
+        public ICollection<Commit> Commits { get; set; } = new List<Commit>();
+    }
+}
