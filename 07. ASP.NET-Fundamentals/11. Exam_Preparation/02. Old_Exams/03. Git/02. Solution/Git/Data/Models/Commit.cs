168fc74baba47077bf0a6a911468625517dc0439
mikegsCoder <mikegsid@gmail.com>
2023-01-15 14:30:22 +0200

    Add Commit Db model

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/03. Git/02. Solution/Git/Data/Models/Commit.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git.Data.Models
+{
+    public class Commit
+    {
+        [Key]
+        [StringLength(36)]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        [Required]
+        [MinLength(5)]
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime CreatedOn { get; set; }
+
+        [ForeignKey(nameof(Creator))]
+        [StringLength(36)]
+        public string CreatorId { get; set; }
+        public User Creator { get; set; }
+
+        [ForeignKey(nameof(Repository))]
+        [StringLength(36)]
+        public string RepositoryId { get; set; }
+        public Repository Repository { get; set; }
+    }
+}
