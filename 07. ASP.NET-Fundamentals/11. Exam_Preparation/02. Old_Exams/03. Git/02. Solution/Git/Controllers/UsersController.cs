90c5426f067fb807ee84d1b0a2acd762085fa428
mikegsCoder <mikegsid@gmail.com>
2023-01-17 18:04:24 +0200

    Add UsersController

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/03. Git/02. Solution/Git/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyWebServer.Controllers;
+using MyWebServer.Http;
+
+namespace Git.Controllers
+{
+    public class UsersController : Controller
+    {
+        public HttpResponse Login()
+        {
+            if (this.User.IsAuthenticated)
+            {
+                return Redirect("/Home/Index");
+            }
+
+            return View();
+        }
+    }
+}
