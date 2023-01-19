c6e95889e441831e885a890c068c2f1b050c08b0
mikegsCoder <mikegsid@gmail.com>
2023-01-19 16:37:49 +0200

    Add ValidationService

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/03. Git/02. Solution/Git/Services/ValidationService/IValidationService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Git.Services.ValidationService
+{
+    public interface IValidationService
+    {
+        ICollection<string> ValidateModel(object model);
+    }
+}
---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/03. Git/02. Solution/Git/Services/ValidationService/ValidationService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace Git.Services.ValidationService
+{
+    public class ValidationService : IValidationService
+    {
+        public ICollection<string> ValidateModel(object model)
+        {
+            var context = new ValidationContext(model);
+            var errors = new List<ValidationResult>();
+
+            Validator.TryValidateObject(model, context, errors, true);
+
+            return errors
+                .Select(e => e.ToString())
+                .ToList();
+        }
+    }
+}
