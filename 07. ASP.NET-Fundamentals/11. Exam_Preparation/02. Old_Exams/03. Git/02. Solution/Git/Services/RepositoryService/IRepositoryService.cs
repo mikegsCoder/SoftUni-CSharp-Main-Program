0844ab26cb61c3512448b1e9201ed64c5586e48e
mikegsCoder <mikegsid@gmail.com>
2023-01-21 11:23:18 +0200

    Add RepositoryService

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/03. Git/02. Solution/Git/Services/RepositoryService/IRepositoryService.cs
@@ -0,0 +1,14 @@
+﻿using Git.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git.Services.RepositoryService
+{
+    public interface IRepositoryService
+    {
+        public AllRepositoriesViewModel GetAllPublic();
+    }
+}
---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/03. Git/02. Solution/Git/Services/RepositoryService/RepositoryService.cs
@@ -0,0 +1,42 @@
+﻿using Git.Data.Common;
+using Git.Data.Models;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Repository = Git.Data.Models.Repository;
+
+namespace Git.Services.RepositoryService
+{
+    public class RepositoryService : IRepositoryService
+    {
+        private readonly IRepository repo;
+
+        public RepositoryService(IRepository _repo)
+        {
+            repo = _repo;
+        }
+
+        public AllRepositoriesViewModel GetAllPublic()
+        {
+            var viewModel = new AllRepositoriesViewModel
+            {
+                AllRepositoryViewModels = repo.All<Repository>()
+                        .Where(r => r.IsPublic)
+                        .Select(r => new RepositoryViewModel()
+                        {
+                            Name = r.Name,
+                            Owner = r.Owner.Username,
+                            CreatedOn = r.CreatedOn.ToString("dd/MM/yyyy hh:mm:ss"),
+                            CommitsCount = r.Commits.Count,
+                            Id = r.Id
+                        })
+                        .ToList()
+            };
+
+            return viewModel;
+        }
+    }
+}
