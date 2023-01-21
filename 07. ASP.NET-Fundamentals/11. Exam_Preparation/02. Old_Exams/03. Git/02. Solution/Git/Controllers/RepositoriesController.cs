5c4587ed77e6593156cc829608b55fe92de34044
mikegsCoder <mikegsid@gmail.com>
2023-01-21 11:20:57 +0200

    Add RepositoriesController

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/03. Git/02. Solution/Git/Controllers/RepositoriesController.cs
@@ -0,0 +1,20 @@
+﻿using MyWebServer.Controllers;
+using MyWebServer.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git.Controllers
+{
+    public class RepositoriesController : Controller
+    {
+        public HttpResponse All()
+        {
+            var model = service.GetAllPublic();
+
+            return View(model);
+        }
+    }
+}
