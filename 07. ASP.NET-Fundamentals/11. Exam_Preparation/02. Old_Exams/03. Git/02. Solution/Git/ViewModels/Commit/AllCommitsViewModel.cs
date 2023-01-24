158ce7b7f2548d5160724c344e6ef43a682149d8
mikegsCoder <mikegsid@gmail.com>
2023-01-24 17:27:35 +0200

    Add AllCommitsViewModel

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/03. Git/02. Solution/Git/ViewModels/Commit/AllCommitsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git.ViewModels.Commit
+{
+    public class AllCommitsViewModel
+    {
+        public IEnumerable<CommitViewModel> AllCommitsViewModels { get; set; }
+    }
+}
