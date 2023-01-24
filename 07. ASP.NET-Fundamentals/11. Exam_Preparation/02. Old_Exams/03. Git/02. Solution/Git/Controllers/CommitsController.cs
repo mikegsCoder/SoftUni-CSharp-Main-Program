86a0fc523d4a2e1d8dfad58d86b052b4dac2d4f3
mikegsCoder <mikegsid@gmail.com>
2023-01-24 17:22:38 +0200

    Add CommitsController

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/03. Git/02. Solution/Git/Controllers/CommitsController.cs
@@ -0,0 +1,30 @@
+﻿using Git.Services.RepositoryService;
+using Git.Services.ValidationService;
+using MyWebServer.Controllers;
+using MyWebServer.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git.Controllers
+{
+    public class CommitsController : Controller
+    {
+        private readonly ICommitService commitService;
+
+        public CommitsController(ICommitService _commitService)
+        {
+            commitService = _commitService;
+        }
+
+        [Authorize]
+        public HttpResponse All()
+        {
+            var models = this.commitService.GetAll(this.User.Id);
+
+            return this.View(models);
+        }
+    }
+}
