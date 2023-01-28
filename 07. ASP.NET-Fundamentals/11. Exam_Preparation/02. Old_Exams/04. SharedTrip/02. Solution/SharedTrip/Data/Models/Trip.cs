71a93e7c6ac3f6793bb87b638c0f0a75920d57d3
mikegsCoder <mikegsid@gmail.com>
2023-01-28 14:10:50 +0200

    Add Trip Db model

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/04. SharedTrip/02. Solution/SharedTrip/Data/Models/Trip.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedTrip.Data.Models
+{
+    public class Trip
+    {
+        [Key]
+        [StringLength(36)]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        [Required]
+        public string StartPoint { get; set; }
+
+        [Required]
+        public string EndPoint { get; set; }
+
+        [Required]
+        public DateTime DepartureTime { get; set; }
+
+        [Required]
+        [Range(2, 6)]
+        public int Seats { get; set; }
+
+        [Required]
+        [MaxLength(80)]
+        public string Description { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public ICollection<UserTrip> UserTrips { get; set; } = new List<UserTrip>();
+    }
+}
