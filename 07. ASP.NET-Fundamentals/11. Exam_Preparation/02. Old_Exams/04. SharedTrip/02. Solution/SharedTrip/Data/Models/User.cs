d14c74c7ea38fa9ca19da15be08558b82a675174
mikegsCoder <mikegsid@gmail.com>
2023-01-28 14:09:49 +0200

    Add User DB model

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/04. SharedTrip/02. Solution/SharedTrip/Data/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedTrip.Data.Models
+{
+    public class User
+    {
+        [Key]
+        [StringLength(36)]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        [Required]
+        [StringLength(20)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(64)]
+        public string Password { get; set; }
+
+        public ICollection<UserTrip> UserTrips { get; set; } = new List<UserTrip>();
+    }
+}
