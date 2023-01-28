c90e4929abd1867e9add8076640a3fbcd2b65526
mikegsCoder <mikegsid@gmail.com>
2023-01-28 14:07:41 +0200

    Add SharedTrip solution initial commit

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/04. SharedTrip/02. Solution/SharedTrip/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿namespace SharedTrip.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           
+        }
+    }
+}
