dc89b17d747e01626204880e7a9f328e8e4aaf36
mikegsCoder <mikegsid@gmail.com>
2023-01-28 14:12:01 +0200

    Add UserTrip Db model

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/04. SharedTrip/02. Solution/SharedTrip/Data/Models/UserTrip.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedTrip.Data.Models
+{
+    public class UserTrip
+    {
+        [StringLength(36)]
+        public string UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public User User { get; set; }
+
+        [StringLength(36)]
+        public string TripId { get; set; }
+
+        [ForeignKey(nameof(TripId))]
+        public Trip Trip { get; set; }
+    }
+}
