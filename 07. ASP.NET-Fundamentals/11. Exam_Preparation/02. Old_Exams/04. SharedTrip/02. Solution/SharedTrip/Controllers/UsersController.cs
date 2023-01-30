e5f427a4d054baec409a6a61b9b343867e402fca
mikegsCoder <mikegsid@gmail.com>
2023-01-30 17:42:19 +0200

    Add UsersController

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/04. SharedTrip/02. Solution/SharedTrip/Controllers/UsersController.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using MyWebServer.Controllers;
+using MyWebServer.Http;
+
+namespace SharedTrip.Controllers
+{
+    public class UsersController : Controller
+    {
+        public HttpResponse Login()
+        {
+            if (this.User.IsAuthenticated)
+            {
+                return Redirect("/Trips/All");
+            }
+
+            return View();
+        }
+    }
+}
