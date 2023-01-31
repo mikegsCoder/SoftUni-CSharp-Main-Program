e3105463c1d3efd70ca3839643414ac7a4ca4bb9
mikegsCoder <mikegsid@gmail.com>
2023-01-31 16:56:42 +0200

    Add Login method

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/04. SharedTrip/02. Solution/SharedTrip/Services/UserService/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using SharedTrip.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedTrip.Services.UserService
+{
+    public interface IUserService
+    {
+        string Login(LoginViewModel model);
+    }
+}
---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/04. SharedTrip/02. Solution/SharedTrip/Services/UserService/UserService.cs
@@ -0,0 +1,37 @@
+﻿using SharedTrip.Data.Models;
+using SharedTrip.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharedTrip.Data.Common;
+using System.Security.Cryptography;
+using System.ComponentModel.DataAnnotations;
+
+namespace SharedTrip.Services.UserService
+{
+    public class UserService : IUserService
+    {
+        private readonly IRepository repository;
+
+
+        public UserService(IRepository _repository)
+        {
+            repository = _repository;
+        }
+
+        public string Login(LoginViewModel model)
+        {
+            var hashedPassword = this.Hash(model.Password);
+
+            var userId = this.repository
+                .All<User>()
+                .Where(u => u.Username == model.Username && u.Password == hashedPassword)
+                .Select(u => u.Id)
+                .FirstOrDefault();
+
+            return userId;
+        }
+    }
+}
