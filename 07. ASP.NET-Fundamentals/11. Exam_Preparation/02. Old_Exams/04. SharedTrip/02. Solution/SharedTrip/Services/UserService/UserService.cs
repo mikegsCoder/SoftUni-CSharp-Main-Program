9bce97ddd7ccdea927fe849e34001a455eba88fb
mikegsCoder <mikegsid@gmail.com>
2023-01-31 16:58:17 +0200

    Add Hash method

--- 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/04. SharedTrip/02. Solution/SharedTrip/Services/UserService/UserService.cs
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/04. SharedTrip/02. Solution/SharedTrip/Services/UserService/UserService.cs
@@ -33,5 +33,25 @@ namespace SharedTrip.Services.UserService
 
             return userId;
         }
+
+        private string Hash(string password)
+        {
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                return string.Empty;
+            }
+
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
     }
 }
