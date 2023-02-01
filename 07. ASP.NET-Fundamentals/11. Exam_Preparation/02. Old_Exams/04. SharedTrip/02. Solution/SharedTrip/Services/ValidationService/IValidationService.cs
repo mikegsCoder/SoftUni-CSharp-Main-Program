192f91794f6cbd038e50cc0909bc50cd6bb232e0
mikegsCoder <mikegsid@gmail.com>
2023-02-01 17:20:33 +0200

    Add ValidationService

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/04. SharedTrip/02. Solution/SharedTrip/Services/ValidationService/IValidationService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace SharedTrip.Services.ValidationService
+{
+    public interface IValidationService
+    {
+        ICollection<string> ValidateModel(object model);
+    }
+}
