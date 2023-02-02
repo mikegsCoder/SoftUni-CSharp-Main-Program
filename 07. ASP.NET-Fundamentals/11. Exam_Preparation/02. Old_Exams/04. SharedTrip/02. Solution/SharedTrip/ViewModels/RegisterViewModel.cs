eaf8cfe626f470668c61d11ed410f54ec8d5b74e
mikegsCoder <mikegsid@gmail.com>
2023-02-02 16:54:23 +0200

    Add RegisterViewModel

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/04. SharedTrip/02. Solution/SharedTrip/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SharedTrip.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [StringLength(20, MinimumLength = 5, ErrorMessage = "{0} must be between {2} and {1} characters")]
+        public string UserName { get; set; }
+
+        [EmailAddress(ErrorMessage = "Email must be valid email")]
+        public string Email { get; set; }
+
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "{0} must be between {2} and {1} characters")]
+        public string Password { get; set; }
+
+        [Compare(nameof(Password), ErrorMessage = "Password and ConfirmPassword must be equal")]
+        public string ConfirmPassword { get; set; }
+    }
+}
