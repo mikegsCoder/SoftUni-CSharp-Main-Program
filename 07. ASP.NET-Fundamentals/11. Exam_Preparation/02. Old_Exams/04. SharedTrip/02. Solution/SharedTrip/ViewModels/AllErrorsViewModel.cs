c5bbb56bc8236937721f2eb9dc542d96e1ddb72d
mikegsCoder <mikegsid@gmail.com>
2023-02-03 17:41:26 +0200

    Add AllErrorsViewModel

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/04. SharedTrip/02. Solution/SharedTrip/ViewModels/AllErrorsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedTrip.ViewModels
+{
+    public class AllErrorsViewModel
+    {
+        public IEnumerable<ErrorViewModel> AllErrorsViewModels { get; set; }
+    }
+}
