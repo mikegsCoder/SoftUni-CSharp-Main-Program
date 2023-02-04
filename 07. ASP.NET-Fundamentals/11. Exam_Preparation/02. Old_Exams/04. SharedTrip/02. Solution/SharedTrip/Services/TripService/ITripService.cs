c8d598692d5039e8a395cf717435f5b09b57ce66
mikegsCoder <mikegsid@gmail.com>
2023-02-04 11:37:46 +0200

    Add TripService

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/04. SharedTrip/02. Solution/SharedTrip/Services/TripService/ITripService.cs
@@ -0,0 +1,14 @@
+﻿using SharedTrip.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedTrip.Services.TripService
+{
+    public interface ITripService
+    {
+        IEnumerable<TripViewModel> GetAll();
+    }
+}
---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/04. SharedTrip/02. Solution/SharedTrip/Services/TripService/TripService.cs
@@ -0,0 +1,37 @@
+﻿using SharedTrip.Data.Common;
+using SharedTrip.Data.Models;
+using SharedTrip.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace SharedTrip.Services.TripService
+{
+    public class TripService : ITripService
+    {
+        private IRepository repository;
+
+        public TripService(IRepository _repository)
+        {
+            repository = _repository;
+        }
+
+        public IEnumerable<TripViewModel> GetAll()
+        {
+            var trips = repository.All<Trip>()
+                .Select(t => new TripViewModel()
+                {
+                    StartPoint = t.StartPoint,
+                    EndPoint = t.EndPoint,
+                    DepartureTime = t.DepartureTime.ToString("dd.MM.yyyy HH:mm"),
+                    Seats = t.Seats,
+                    Id = t.Id,
+                    Description = t.Description
+                })
+                .ToList();
+
+            return trips;
+        }
+    }
+}
