a75ce5aa52f811c34ea9228ea1e4d5d56338ed85
mikegsCoder <mikegsid@gmail.com>
2023-02-04 11:40:26 +0200

    Add AllTripsViewModel

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/04. SharedTrip/02. Solution/SharedTrip/ViewModels/AllTripsViewModels.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedTrip.ViewModels
+{
+    public class AllTripsViewModels
+    {
+        public IEnumerable<TripViewModel> TripsViewModels { get; set; }
+    }
+}
