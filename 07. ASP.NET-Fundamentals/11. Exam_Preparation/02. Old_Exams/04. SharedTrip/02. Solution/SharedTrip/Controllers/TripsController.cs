30ec7b5df7da1d2b8557c36589ef5d3d0d286bae
mikegsCoder <mikegsid@gmail.com>
2023-02-04 11:36:01 +0200

    Add TripsController

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/04. SharedTrip/02. Solution/SharedTrip/Controllers/TripsController.cs
@@ -0,0 +1,29 @@
+﻿using MyWebServer.Controllers;
+using MyWebServer.Http;
+using SharedTrip.Services.TripService;
+using SharedTrip.ViewModels;
+using System.Linq;
+
+namespace SharedTrip.Controllers
+{
+    public class TripsController : Controller
+    {
+        private ITripService service;
+
+        public TripsController(ITripService _service) 
+        {   
+            service = _service;
+        }
+
+        [Authorize]
+        public HttpResponse All()
+        {
+            var trips = new AllTripsViewModels()
+            { 
+                TripsViewModels = service.GetAll() 
+            };
+
+            return View(trips);
+        }
+    }
+}
