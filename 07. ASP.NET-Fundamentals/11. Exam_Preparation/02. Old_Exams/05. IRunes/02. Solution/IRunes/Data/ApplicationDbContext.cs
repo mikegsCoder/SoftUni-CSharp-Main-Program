c9d74ac7cfa7d0efb35d0b7e3ae1e332285cd5f5
mikegsCoder <mikegsid@gmail.com>
2023-02-09 17:40:17 +0200

    Add IRunes initial commit

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/05. IRunes/02. Solution/IRunes/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IRunes.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext()
+        { 
+        }
+        public ApplicationDbContext(DbContextOptions db)
+            : base(db)
+        { 
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer("Server=.;Database=IRunes;Integrated Security=true;");
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/05. IRunes/02. Solution/IRunes/Startup.cs
@@ -0,0 +1,19 @@
+﻿using SUS.HTTP;
+using IRunes.Data;
+using SUS.MvcFramework;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace IRunes
+{
+    public class Startup : IMvcApplication
+    {
+        public void ConfigureServices(IServiceCollection serviceCollection)
+        {
+        }
+
+        public void Configure(List<Route> routeTable)
+        {
+        }
+    }
+}
