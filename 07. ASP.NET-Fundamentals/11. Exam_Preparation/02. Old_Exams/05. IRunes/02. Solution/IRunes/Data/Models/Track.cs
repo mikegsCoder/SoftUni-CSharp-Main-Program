913675807ceae27e3150e7cd0482f938564f11dd
mikegsCoder <mikegsid@gmail.com>
2023-02-10 17:59:40 +0200

    Add Track Db model

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/05. IRunes/02. Solution/IRunes/Data/Models/Track.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace IRunes.Data.Models
+{
+    public class Track
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        [Required]
+        [MaxLength(20)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Link { get; set; }
+
+        public decimal Price { get; set; }
+
+        [Required]
+        public string AlbumId { get; set; }
+
+        public virtual Album Album { get; set; }
+    }
+}
