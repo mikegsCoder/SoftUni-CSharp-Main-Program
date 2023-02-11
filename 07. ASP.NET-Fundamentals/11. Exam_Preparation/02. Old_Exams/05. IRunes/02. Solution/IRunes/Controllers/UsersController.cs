530ddc367606e6cbeb5038e45d0573647c0b6eba
mikegsCoder <mikegsid@gmail.com>
2023-02-11 08:21:39 +0200

    Add UsersController

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/05. IRunes/02. Solution/IRunes/Controllers/UsersController.cs
@@ -0,0 +1,19 @@
+﻿using SUS.HTTP;
+using SUS.MvcFramework;
+using System.ComponentModel.DataAnnotations;
+
+namespace IRunes.Controllers
+{
+    public class UsersController : Controller
+    {
+        public HttpResponse Login()
+        {
+            if (IsUserSignedIn())
+            {
+                return Redirect("/");
+            }
+
+            return View();
+        }
+    }
+}
