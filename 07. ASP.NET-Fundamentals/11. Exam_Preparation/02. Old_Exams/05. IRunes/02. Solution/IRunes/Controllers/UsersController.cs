7fba75d540d79a2d62afa3345da4afefb0f9ad05
mikegsCoder <mikegsid@gmail.com>
2023-02-13 18:05:52 +0200

    Add HttpPost Register

--- 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/05. IRunes/02. Solution/IRunes/Controllers/UsersController.cs
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/05. IRunes/02. Solution/IRunes/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using SUS.HTTP;
+﻿using IRunes.ViewModels.Users;
+using SUS.HTTP;
 using SUS.MvcFramework;
 using System.ComponentModel.DataAnnotations;
 
@@ -24,6 +25,54 @@ namespace IRunes.Controllers
             }
 
             return View();
+        }
+
+        [HttpPost]
+        public HttpResponse Register(RegisterInputModel register)
+        {
+            if (IsUserSignedIn())
+            {
+                return Redirect("/");
+            }
+
+            if (string.IsNullOrEmpty(register.Username)
+                || register.Username.Length < 4
+                || register.Username.Length > 10)
+            {
+                return Error("Name should be between 5 and 20 characters");
+            }
+
+            if (string.IsNullOrEmpty(register.Email)
+                || !new EmailAddressAttribute().IsValid(register.Email))
+            {
+                return Error("Email is required");
+            }
+
+            if (string.IsNullOrEmpty(register.Password)
+                || register.Password.Length < 6
+                || register.Password.Length > 20)
+            {
+                return Error("Password should be between 6 and 20 characters");
+            }
+
+            if (!this.usersService.IsUsernameAvailable(register))
+            {
+                return Error("Username not available");
+            }
+
+            if (!this.usersService.IsEmailAvailable(register))
+            {
+                return Error("Email not available");
+            }
+
+            if (register.ConfirmPassword != register.Password)
+            {
+                return Error("Passwords do not match");
+            }
+
+            this.usersService.Create(register);
+
+            return Redirect("/Users/Login");
         }
     }
 }
