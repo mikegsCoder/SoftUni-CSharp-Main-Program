805774189275dda437282e8888f956cd1051f3fc
mikegsCoder <mikegsid@gmail.com>
2023-02-15 17:18:34 +0200

    Modified HomeController

--- 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/05. IRunes/02. Solution/IRunes/Controllers/HomeController.cs
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/05. IRunes/02. Solution/IRunes/Controllers/HomeController.cs
@@ -1,14 +1,31 @@
-﻿using SUS.HTTP;
+﻿using IRunes.Services.Users;
+using SUS.HTTP;
 using SUS.MvcFramework;
 
 namespace IRunes.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IUsersService usersService;
+
+        public HomeController(IUsersService _usersService)
+        {
+            usersService = _usersService;
+        }
+
         [HttpGet("/")]
         public HttpResponse Index()
         {
-            return this.View();
+            if (IsUserSignedIn())
+            {
+                var userId = GetUserId();
+
+                var viewModel = usersService.GetHomeViewModel(userId);
+
+                return View(viewModel, "/Home");
+            }
+
+            return View();
         }
     }
 }
