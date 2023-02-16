108d00fd337a1f7b2c024271ad4d2f0469d9fba4
mikegsCoder <mikegsid@gmail.com>
2023-02-16 16:53:28 +0200

    Add AlbumsController

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/05. IRunes/02. Solution/IRunes/Controllers/AlbumsController.cs
@@ -0,0 +1,20 @@
+﻿using SUS.HTTP;
+using SUS.MvcFramework;
+
+namespace IRunes.Controllers
+{
+    public class AlbumsController : Controller
+    {
+        public HttpResponse All()
+        {
+            if (!IsUserSignedIn())
+            {
+                return Redirect("/");
+            }
+
+            var viewModel = albumsService.GetAllAlbums();
+
+            return View(viewModel);
+        }
+    }
+}
