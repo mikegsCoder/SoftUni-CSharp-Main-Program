fb93224171abd07da2c493074d70160a931e61e7
mikegsCoder <mikegsid@gmail.com>
2023-02-23 16:49:13 +0200

    Add TracksController

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/05. IRunes/02. Solution/IRunes/Controllers/TracksController.cs
@@ -0,0 +1,20 @@
+﻿using SUS.HTTP;
+using SUS.MvcFramework;
+
+namespace IRunes.Controllers
+{
+    public class TracksController : Controller
+    {
+        public HttpResponse Create(string albumId)
+        {
+            if (!IsUserSignedIn())
+            {
+                return Redirect("/");
+            }
+
+            var viewModel = tracksService.GetTrackViewModel(albumId);
+
+            return View(viewModel);
+        }
+    }
+}
