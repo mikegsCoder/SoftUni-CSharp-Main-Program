2a7037b44d96e922ba20562e8b298ebe1cf6cd93
mikegsCoder <mikegsid@gmail.com>
2023-02-23 16:52:02 +0200

    Add GetTrackViewModel method

---
+++ 07. ASP.NET-Fundamentals/11. Exam_Preparation/02. Old_Exams/05. IRunes/02. Solution/IRunes/Services/Tracks/TracksService.cs
@@ -0,0 +1,27 @@
+﻿using IRunes.Data;
+using IRunes.Data.Models;
+using System;
+using System.Linq;
+
+namespace IRunes.Services.Tracks
+{
+    public class TracksService : ITracksService
+    {
+        private readonly ApplicationDbContext db;
+
+        public TracksService(ApplicationDbContext _db)
+        {
+            db = _db;
+        }
+
+        public CreateTrackViewModel GetTrackViewModel(string albumId)
+        {
+            var viewModel = new CreateTrackViewModel
+            {
+                AlbumId = albumId
+            };
+
+            return viewModel;
+        }
+    }
+}
